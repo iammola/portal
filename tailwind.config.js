d97ad4a74077a88ae744f4e5d1679dec03ced911
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-25 13:40:18 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    content: ["pages/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+    theme: {},
+    variants: {},
+    plugins: [],
+};
