0c4966958e686239bf2cdaae4b4ab833b5be2016
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-25 19:40:09 +0100

    added function to connect to db

---
+++ db/index.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI ?? "";
+
+// Global is used here to maintain a cached connection across hot reloads in development. This prevents connections growing exponentially during API Route usage.
+const cached = global.mongoose ?? {};
+
+export async function connect(): Promise<typeof mongoose> {
+    cached.promise ??= mongoose.connect(MONGODB_URI, {
+        bufferCommands: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    } as ConnectOptionsExt);
+
+    return (cached.conn ??= await cached.promise);
+}
+
+interface ConnectOptionsExt extends mongoose.ConnectOptions {
+    /** False by default. If `true`, this connection will use `createIndex()` instead of `ensureIndex()` for automatic index builds via `Model.init()`. */
+    useCreateIndex?: boolean;
+    /** false by default. Set to `true` to make all connections set the `useNewUrlParser` option by default */
+    useNewUrlParser?: boolean;
+    /** false by default. Set to `true` to make all connections set the `useUnifiedTopology` option by default */
+    useUnifiedTopology?: boolean;
+}
+
+declare const global: {
+    mongoose: {
+        conn: typeof mongoose;
+        promise: Promise<typeof mongoose>;
+    };
+};
