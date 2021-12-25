c44fc176690eb8830f1f42473d8611c287cb48ea
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-25 21:03:26 +0100

    added `SubjectDivisionSchema` interface for subjects with divisons

--- types/schema/Subject.ts
+++ types/schema/Subject.ts
@@ -1,10 +1,18 @@
-import { ObjectId, DocumentId } from "types/schema";
+import type { ObjectId, DocumentId } from "types/schema";
 
-export interface SubjectSchema extends DocumentId {
+export interface SubjectDivisionSchema extends DocumentId {
     name: string;
     alias: string;
+    // Note: If undefined, subject has divisions
+    teachers?: ObjectId[];
+}
+
+export interface SubjectSchema extends DocumentId, SubjectDivisionSchema {
     class: ObjectId;
-    teachers: ObjectId[];
+    // Note: If undefined, subject is not required
+    required?: true;
     // Note: If undefined, subject exists for all sessions
     sessions?: ObjectId[];
+    // Note: If undefined, subject doesn't have divisions
+    divisions?: SubjectDivisionSchema[];
 }
