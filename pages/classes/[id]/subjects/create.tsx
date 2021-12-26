9ed5f955b9c2662686ff349277ad37798433b7c3
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-26 01:18:40 +0100

    added subjects create page title

---
+++ pages/classes/[id]/subjects/create.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+import type { NextPage } from "next";
+
+const CreateSubject: NextPage = () => {
+    return (
+        <main className="flex items-center justify-center w-screen h-screen">
+            <Head>
+                <title>Create Subject | Portal | GRS™</title>
+                <meta name="description" content="Page for Subject Creation" />
+            </Head>
+        </main>
+    );
+};
+
+export default CreateSubject;
