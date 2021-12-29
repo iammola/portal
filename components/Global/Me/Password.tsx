aa3c3362a929047ab7992de28b1c4f19e8974605
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-29 19:32:26 +0100

    added password fields

--- components/Global/Me/Password.tsx
+++ components/Global/Me/Password.tsx
@@ -9,6 +9,45 @@ const Password: FunctionComponent = () => {
                     Please enter your current password to change your password.
                 </p>
             </div>
+            <div className="grid grid-cols-4 gap-20 w-full py-7">
+                <h4 className="w-max min-w-max text-sm text-gray-700 font-medium col-start-1 col-end-2">
+                    Current password
+                </h4>
+                <div className="flex flex-col gap-2 items-start justify-start w-[30rem] col-start-2 col-end-4">
+                    <input
+                        required
+                        type="password"
+                        className="text-slate-800 text-xl font-extrabold rounded-md shadow shadow-gray-300 transition-shadow focus:ring-2 focus:ring-gray-400 focus:outline-none w-full py-3 px-4"
+                    />
+                </div>
+            </div>
+            <div className="grid grid-cols-4 gap-20 w-full py-7">
+                <h4 className="w-max min-w-max text-sm text-gray-700 font-medium col-start-1 col-end-2">
+                    New password
+                </h4>
+                <div className="flex flex-col gap-2 items-start justify-start w-[30rem] col-start-2 col-end-4">
+                    <input
+                        required
+                        type="password"
+                        className="text-slate-800 text-xl font-extrabold rounded-md shadow shadow-gray-300 transition-shadow focus:ring-2 focus:ring-gray-400 focus:outline-none w-full py-3 px-4"
+                    />
+                    <span className="text-xs text-gray-700 tracking-wide">
+                        Your new password must be more than 8 characters.
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 gap-20 w-full py-7">
+                <h4 className="w-max min-w-max text-sm text-gray-700 font-medium col-start-1 col-end-2">
+                    Confirm new password
+                </h4>
+                <div className="flex flex-col gap-2 items-start justify-start w-[30rem] col-start-2 col-end-4">
+                    <input
+                        required
+                        type="password"
+                        className="text-slate-800 text-xl font-extrabold rounded-md shadow shadow-gray-300 transition-shadow focus:ring-2 focus:ring-gray-400 focus:outline-none w-full py-3 px-4"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
