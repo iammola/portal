49a5645dc9c90281927c2cb090c7e8764f8931e3
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-29 19:31:08 +0100

    added component title and description

--- components/Global/Me/Password.tsx
+++ components/Global/Me/Password.tsx
@@ -1,7 +1,16 @@
 import { FunctionComponent } from "react";
 
 const Password: FunctionComponent = () => {
-    return <></>;
+    return (
+        <div className="flex flex-col gap-y-4 w-full divide-y-2 border-gray-200">
+            <div>
+                <h5 className="text-2xl text-slate-800 font-semibold tracking-wide">Password</h5>
+                <p className="text-sm text-gray-500">
+                    Please enter your current password to change your password.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Password;
