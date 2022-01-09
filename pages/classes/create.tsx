acf5fc030fbb43ba1022bfd327e204a6a0c92f0b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-09 21:17:43 +0100

    added page heading

--- pages/classes/create.tsx
+++ pages/classes/create.tsx
@@ -9,6 +9,14 @@ const CreateClass: NextPage = () => {
                 <title>Create Class | GRIS Portal</title>
                 <meta name="description" content="Create class" />
             </Head>
+            <section className="flex flex-col items-start justify-start flex-grow w-full">
+                <h1 className="text-5xl font-semibold text-gray-600 pt-10 px-5">
+                    <span>Create a</span>{" "}
+                    <span className="bg-clip-text bg-gradient-to-br from-emerald-300 to-emerald-600 text-transparent">
+                        Class
+                    </span>
+                </h1>
+            </section>
         </main>
     );
 };
