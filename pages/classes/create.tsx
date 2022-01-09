35a5e9a19ca34ea99cacd2912c56faae16d3d06e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-09 21:31:27 +0100

    added colors to background and heading for dark mode

--- pages/classes/create.tsx
+++ pages/classes/create.tsx
@@ -4,13 +4,13 @@ import type { NextPage } from "next";
 
 const CreateClass: NextPage = () => {
     return (
-        <main className="flex flex-row items-stretch justify-center w-screen h-full min-h-screen bg-slate-50">
+        <main className="flex flex-row items-stretch justify-center w-screen h-full min-h-screen bg-slate-50 dark:bg-slate-900">
             <Head>
                 <title>Create Class | GRIS Portal</title>
                 <meta name="description" content="Create class" />
             </Head>
             <section className="flex flex-col items-start justify-start flex-grow w-full">
-                <h1 className="text-5xl font-semibold text-gray-600 p-10">
+                <h1 className="text-5xl font-semibold text-gray-600 dark:text-gray-300 p-10">
                     <span>Create a</span>{" "}
                     <span className="bg-clip-text bg-gradient-to-br from-emerald-300 to-emerald-600 text-transparent">
                         Class
