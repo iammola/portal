540acdc70c435cd96e899fa726cc7fc9edd7e724
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-09 21:06:04 +0100

    added Head component

--- pages/classes/create.tsx
+++ pages/classes/create.tsx
@@ -1,7 +1,16 @@
+import Head from "next/head";
+
 import type { NextPage } from "next";
 
 const CreateClass: NextPage = () => {
-    return <></>;
+    return (
+        <main className="flex flex-row items-stretch justify-center w-screen h-full min-h-screen bg-slate-50">
+            <Head>
+                <title>Create Class | GRIS Portal</title>
+                <meta name="description" content="Create class" />
+            </Head>
+        </main>
+    );
 };
 
 export default CreateClass;
