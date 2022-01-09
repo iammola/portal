f78c2aae7c820ebc25c41ab3c3fc422795fa53fb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-09 21:27:27 +0100

    increased the heading's padding

--- pages/classes/create.tsx
+++ pages/classes/create.tsx
@@ -10,7 +10,7 @@ const CreateClass: NextPage = () => {
                 <meta name="description" content="Create class" />
             </Head>
             <section className="flex flex-col items-start justify-start flex-grow w-full">
-                <h1 className="text-5xl font-semibold text-gray-600 pt-10 px-5">
+                <h1 className="text-5xl font-semibold text-gray-600 p-10">
                     <span>Create a</span>{" "}
                     <span className="bg-clip-text bg-gradient-to-br from-emerald-300 to-emerald-600 text-transparent">
                         Class
