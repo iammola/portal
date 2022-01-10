984ac69fa188f9a447a1058353f7f9546892cdf6
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-10 20:43:08 +0100

    created Input interface for components

--- components/Global/Input/Box/index.tsx
+++ components/Global/Input/Box/index.tsx
@@ -1,7 +1,21 @@
 import { FunctionComponent } from "react";
 
-const Input: FunctionComponent = () => {
+import Main from "./Main";
+import Badge from "./Badge";
+import Label from "./Label";
+
+const Input: Input = () => {
     return <></>;
 };
+
+Input.Main = Main;
+Input.Badge = Badge;
+Input.Label = Label;
+
+interface Input extends FunctionComponent {
+    Main: Main;
+    Badge: Badge;
+    Label: Label;
+}
 
 export default Input;
