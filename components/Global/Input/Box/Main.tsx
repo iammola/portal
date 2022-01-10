52ea77cd200336e9c628d1741e0c0bd9bbf311eb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-10 22:46:14 +0100

    added keyDown event

--- components/Global/Input/Box/Main.tsx
+++ components/Global/Input/Box/Main.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, KeyboardEvent } from "react";
 
 type Value = { [K in "_id" | "value"]: string };
 
@@ -8,10 +8,24 @@ type Main = FunctionComponent<{
     removeValue(v: Value["_id"]): void;
 }>;
 
-const Main: Main = ({ values }) => {
+const Main: Main = ({ addValue, values }) => {
+    function keyEvents(e: KeyboardEvent<HTMLElement>) {
+        if (e.key === "Enter") {
+            const lastChild = (e.target as HTMLElement).lastChild;
+            const value = lastChild?.textContent?.trim();
+            e.preventDefault();
+
+            if (value != undefined && values.find((item) => item.value === value) === undefined) {
+                lastChild?.remove();
+                addValue({ _id: "", value });
+            }
+        }
+    }
+
     return (
         <div
             contentEditable
+            onKeyDown={keyEvents}
             suppressContentEditableWarning
             className="w-full h-full border-4"
         >
