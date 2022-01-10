09e8779563af616bc546c0529b1a46230ae5979b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-10 22:43:32 +0100

    used `values` prop  to render list of items

--- components/Global/Input/Box/Main.tsx
+++ components/Global/Input/Box/Main.tsx
@@ -8,8 +8,18 @@ type Main = FunctionComponent<{
     removeValue(v: Value["_id"]): void;
 }>;
 
-const Main: Main = () => {
-    return <div contentEditable suppressContentEditableWarning />;
+const Main: Main = ({ values }) => {
+    return (
+        <div
+            contentEditable
+            suppressContentEditableWarning
+            className="w-full h-full border-4"
+        >
+            {values.map((item) => (
+                <li key={item._id}>{item.value}</li>
+            ))}
+        </div>
+    );
 };
 
 export default Main;
