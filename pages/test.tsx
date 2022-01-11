5d30517605c3358e73d94a4b38c78e6276fb40c1
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-11 02:03:06 +0100

    added styles to elements

--- pages/test.tsx
+++ pages/test.tsx
@@ -9,8 +9,13 @@ const InputTest: NextPage = () => {
                 <title>Input Component Test</title>
             </Head>
             <div className="relative">
-                <label htmlFor="input"></label>
-                <input id="input" type="text" name="input" />
+                <label htmlFor="input" className="text-gray-800 font-medium"></label>
+                <input
+                    id="input"
+                    type="text"
+                    name="input"
+                    className="w-full border border-gray-300 rounded-md overflow-hidden focus:outline-none"
+                />
             </div>
         </main>
     );
