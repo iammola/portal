cac2c89293f4a5ab6f764df6dd07c2cd11d6ec6e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-11 03:16:19 +0100

    added styles to trigger floating label

--- pages/test.tsx
+++ pages/test.tsx
@@ -14,11 +14,11 @@ const InputTest: NextPage = () => {
                     type="email"
                     name="input"
                     placeholder="Email address"
-                    className="peer w-full h-[3.75rem] p-2 pl-4 border border-gray-400 rounded-lg overflow-hidden focus:outline-none"
+                    className="peer w-full h-[3.75rem] p-2 pl-4 border border-gray-400 rounded-lg overflow-hidden focus:outline-none placeholder-transparent"
                 />
                 <label
                     htmlFor="input"
-                    className="absolute left-0 -top-6 text-sm text-gray-600 font-semibold"
+                    className="absolute left-3 p-1 text-gray-800 transition-all text-sm -top-3.5 font-medium bg-white tracking-wide peer-focus:text-sm peer-focus:-top-3.5 peer-focus:bg-white peer-focus:font-medium peer-focus:tracking-wide peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base peer-placeholder-shown:font-normal peer-placeholder-shown:bg-transparent peer-placeholder-shown:tracking-normal"
                 >
                     Email address
                 </label>
