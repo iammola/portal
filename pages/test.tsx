ab280bbfd4d22b68aae26d85291cdd0285d41c6c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-11 02:10:44 +0100

    changed styles

--- pages/test.tsx
+++ pages/test.tsx
@@ -4,19 +4,19 @@ import type { NextPage } from "next";
 
 const InputTest: NextPage = () => {
     return (
-        <main className="flex flex-row items-center justify-center w-screen h-screen bg-slate-900">
+        <main className="flex flex-row items-center justify-center w-screen h-screen">
             <Head>
                 <title>Input Component Test</title>
             </Head>
-            <div className="relative">
-                <label htmlFor="input" className="text-gray-800 font-medium">
+            <div className="relative w-[20rem] flex flex-col gap-y-2 items-start justify-start">
+                <label htmlFor="input" className="text-gray-800 font-medium tracking-wide">
                     Email address
                 </label>
                 <input
                     id="input"
                     type="email"
                     name="input"
-                    className="w-full border border-gray-300 rounded-md overflow-hidden focus:outline-none"
+                    className="w-full h-14 p-2 pl-6 border border-gray-300 rounded-lg overflow-hidden focus:outline-none"
                 />
             </div>
         </main>
