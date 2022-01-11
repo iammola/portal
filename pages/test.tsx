fead06a4ceda6bef8e4ab4a5812a8854e012b6ea
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-11 02:12:23 +0100

    moved label after input for peer styles

--- pages/test.tsx
+++ pages/test.tsx
@@ -9,15 +9,15 @@ const InputTest: NextPage = () => {
                 <title>Input Component Test</title>
             </Head>
             <div className="relative w-[20rem] flex flex-col gap-y-2 items-start justify-start">
-                <label htmlFor="input" className="text-gray-800 font-medium tracking-wide">
-                    Email address
-                </label>
                 <input
                     id="input"
                     type="email"
                     name="input"
                     className="w-full h-14 p-2 pl-6 border border-gray-300 rounded-lg overflow-hidden focus:outline-none"
                 />
+                <label htmlFor="input" className="text-gray-800 font-medium tracking-wide">
+                    Email address
+                </label>
             </div>
         </main>
     );
