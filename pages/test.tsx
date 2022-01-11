634952c68e1d5f021308f345be2b9193f8ced712
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-11 01:32:14 +0100

    initial commit of input design test

---
+++ pages/test.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+import type { NextPage } from "next";
+
+const InputTest: NextPage = () => {
+    return (
+        <main className="flex flex-row items-center justify-center w-screen h-screen bg-slate-900">
+            <Head>
+                <title>Input Component Test</title>
+            </Head>
+        </main>
+    );
+};
+
+export default InputTest;
