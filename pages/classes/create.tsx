13f24112d8ec5fdca687a31a0aaa4ed4c1a0a7f8
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-12 19:26:05 +0100

    added `name` Input component

--- pages/classes/create.tsx
+++ pages/classes/create.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import Head from "next/head";
+
+import Input from "components/Global/Input";
+import { classNames } from "utils";
 
 import type { NextPage } from "next";
 
 const CreateClass: NextPage = () => {
+    const [name, setName] = useState("");
+
     return (
         <main className="flex flex-row items-stretch justify-center w-screen h-full min-h-screen bg-slate-50 dark:bg-slate-900">
             <Head>
@@ -17,7 +23,27 @@ const CreateClass: NextPage = () => {
                     </span>
                 </h1>
                 <div className="flex flex-row items-center justify-center self-center h-full py-4">
-                    <form className="w-[60rem] h-[35rem] bg-slate-50 dark:bg-slate-300 rounded-2xl overflow-hidden shadow-lg" />
+                    <form className="w-[60rem] h-[35rem] bg-white p-10 rounded-2xl overflow-hidden shadow-lg">
+                        <Input
+                            required
+                            id="name"
+                            type="text"
+                            value={name}
+                            label="Name"
+                            onChange={(v) => setName(v as string)}
+                            className={(valid) =>
+                                classNames(
+                                    "w-[20rem] h-[3.75rem] border placeholder-shown:border-gray-400 rounded-md overflow-hidden focus:outline-none ring-2 focus:ring-blue-400 placeholder-shown:!ring-transparent placeholder-transparent",
+                                    {
+                                        "valid:ring-emerald-400 focus:valid:ring-emerald-400":
+                                            valid === true,
+                                        "invalid:ring-red-400 focus:invalid:ring-red-400":
+                                            valid === false,
+                                    }
+                                )
+                            }
+                        />
+                    </form>
                 </div>
             </section>
         </main>
