59700037b26cbde1a2cf87a2e50c967cf5393c20
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-14 16:54:07 +0100

    preferred `Optional` over `*` to indicate required fields over

--- components/Global/Input/index.tsx
+++ components/Global/Input/index.tsx
@@ -41,10 +41,10 @@ const Input: Input = ({ className, label, showIcons, onChange, ...props }) => {
                 className="absolute left-[-.4rem] p-1 text-gray-800 transition-all text-sm -top-3.5 font-medium bg-white tracking-wide peer-focus:text-sm peer-focus:left-[-.4rem] peer-focus:top-[-.95rem] peer-focus:bg-white peer-focus:font-medium peer-focus:tracking-wide peer-placeholder-shown:select-none peer-placeholder-shown:left-3 peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base peer-placeholder-shown:font-normal peer-placeholder-shown:bg-transparent peer-placeholder-shown:tracking-normal"
             >
                 {label}
-                {props.required === true && (
-                    <span className="text-sm text-red-500 pl-0.5 align-middle">*</span>
-                )}
             </label>
+            {props.required !== true && (
+                <span className="absolute right-0.5 -top-5 text-xs text-gray-500">Optional</span>
+            )}
             {showIcons === true && (
                 <>
                     <CheckIcon
