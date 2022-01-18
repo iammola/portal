3111ca810714c84b5fb3aeb64aeb8491c7240d5e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-18 02:29:07 +0100

    created `ModelRecord` type
    The ModelRecord type is to help with type assertions when using virtual properties with the mongoose-lean-virtuals package.
    
    It requires 2-3 arguments,
    1. The S is required and represents the model’s schema.
    2. The V is required and represents the model’s virtual properties.
    3. The K is optional and represents what properties from the virtual object will be used.
         - If true, then all the virtual properties are merged with the schema.
         - If false, then none of the virtual properties are merged with the schema. 
        - If a string union is provided, I use the native Pick type to select that key’s type from the virtual properties provided.

--- types/schema/index.ts
+++ types/schema/index.ts
@@ -1,11 +1,15 @@
 import type { Schema } from "mongoose";
 import type { ObjectId as BSONId } from "bson";
+import type { FlattenIntersection } from "types/utils";
 
 export type ObjectId = BSONId & Schema.Types.ObjectId;
 
 export interface DocumentId {
     _id: ObjectId;
 }
+
+export type ModelRecord<S, V, K extends boolean | keyof V = false> = S &
+    (K extends true ? V : FlattenIntersection<K extends keyof V ? Pick<V, K> : unknown>);
 
 export * from "./Class";
 export * from "./Subject";
