f89e4b33e9069844a921b75b41f38bb99246ebfc
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-21 08:04:59 +0100

    added `teachers` property to request body

--- types/api/classes/index.ts
+++ types/api/classes/index.ts
@@ -1,4 +1,4 @@
 import type { ClassSchema } from "types/schema";
 
 export type CreateClassData = Pick<ClassSchema, "_id" | "createdAt">;
-export type CreateClassRequestBody = Pick<ClassSchema, "name" | "alias" | "special">;
+export type CreateClassRequestBody = Pick<ClassSchema, "name" | "alias" | "special" | "teachers">;
