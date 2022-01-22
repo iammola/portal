f1b04ffa3fd3f50987dd073b5b7a8d1dbf16e41a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-22 03:29:07 +0100

    added `valid` state and `typing` hook

--- components/Global/Input/Textarea/Field.tsx
+++ components/Global/Input/Textarea/Field.tsx
@@ -1,11 +1,28 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 
-const Field: Field = ({ value, onChange }) => {
-    return <textarea value={value} onChange={(e) => onChange(e.target.value)} />;
+import { useIsChanging } from "hooks";
+
+const Field: Field = ({ className, onChange, required, value }) => {
+    const typing = useIsChanging(value);
+    const [valid, setValid] = useState<boolean>();
+
+    useEffect(() => {
+        if (typing === false)
+            setValid(required === true ? value.length > 0 : value.length < 1 ? undefined : true);
+    }, [required, typing, value]);
+
+    return (
+        <textarea
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+            className={typeof className === "string" ? className : className(valid)}
+        />
+    );
 };
 
 type Field = FunctionComponent<{
     value: string;
+    required?: boolean;
     onChange(values: string): void;
     className: string | ((valid?: boolean) => string);
 }>;
