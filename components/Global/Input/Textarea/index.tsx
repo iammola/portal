dff60aeca95b8cb9d8d35a9e19d67cf3d33909f6
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-22 02:55:51 +0100

    moved Textarea file

---
+++ components/Global/Input/Textarea/index.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent } from "react";
+
+import Field from "./Field";
+
+const Textarea: Textarea = ({ children, className }) => {
+    return <div className={className}>{children}</div>;
+};
+
+Textarea.Field = Field;
+
+Textarea.Label = ({ children, className }) => {
+    return <span className={className}>{children}</span>;
+};
+
+type Label = FunctionComponent<{ className: string }>;
+
+interface Textarea extends FunctionComponent<{ className?: string }> {
+    Field: Field;
+    Label: Label;
+}
+
+export default Textarea;
