3b38c09eb95d842bfc6ae6f8c751946b5cea3231
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-24 03:02:24 +0100

    added month and year fields

--- components/Global/Input/Date/index.tsx
+++ components/Global/Input/Date/index.tsx
@@ -5,6 +5,8 @@ import { classNames } from "utils";
 
 const DateInput: DateInput = ({ className }) => {
     const [day, setDay] = useState<number>();
+    const [year, setYear] = useState<number>();
+    const [month, setMonth] = useState<number>();
 
     return (
         <div className={className}>
@@ -17,6 +19,48 @@ const DateInput: DateInput = ({ className }) => {
                     label="Day"
                     value={day}
                     onChange={setDay}
+                    className={(valid?: boolean) =>
+                        classNames(
+                            "w-full h-[3.75rem] border placeholder-shown:border-slate-300 focus:border-transparent focus:valid:border-transparent focus:invalid:border-transparent rounded-lg overflow-hidden focus:outline-none ring-2 focus:ring-blue-400 placeholder-shown:ring-transparent placeholder-transparent [-webkit-appearance:none]",
+                            {
+                                "valid:ring-emerald-400 focus:valid:ring-emerald-400":
+                                    valid === true,
+                                "invalid:ring-red-400 focus:invalid:ring-red-400": valid === false,
+                            }
+                        )
+                    }
+                />
+            </div>
+            <div className="w-[5rem]">
+                <Input.Number
+                    required
+                    min={1}
+                    max={12}
+                    id="month"
+                    label="Month"
+                    value={month}
+                    onChange={setMonth}
+                    className={(valid?: boolean) =>
+                        classNames(
+                            "w-full h-[3.75rem] border placeholder-shown:border-slate-300 focus:border-transparent focus:valid:border-transparent focus:invalid:border-transparent rounded-lg overflow-hidden focus:outline-none ring-2 focus:ring-blue-400 placeholder-shown:ring-transparent placeholder-transparent [-webkit-appearance:none]",
+                            {
+                                "valid:ring-emerald-400 focus:valid:ring-emerald-400":
+                                    valid === true,
+                                "invalid:ring-red-400 focus:invalid:ring-red-400": valid === false,
+                            }
+                        )
+                    }
+                />
+            </div>
+            <div className="w-[6.5rem]">
+                <Input.Number
+                    min={0}
+                    id="year"
+                    required
+                    max={9999}
+                    label="Year"
+                    value={year}
+                    onChange={setYear}
                     className={(valid?: boolean) =>
                         classNames(
                             "w-full h-[3.75rem] border placeholder-shown:border-slate-300 focus:border-transparent focus:valid:border-transparent focus:invalid:border-transparent rounded-lg overflow-hidden focus:outline-none ring-2 focus:ring-blue-400 placeholder-shown:ring-transparent placeholder-transparent [-webkit-appearance:none]",
