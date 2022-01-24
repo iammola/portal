5f43fc48ee950ce2f3cffc48232ee19d474d6888
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-24 01:17:14 +0100

    specified `type` prop

--- components/Global/Input/Number.tsx
+++ components/Global/Input/Number.tsx
@@ -6,11 +6,19 @@ const NumberInput: NumberInput = (props) => {
     const validateCharacter = (e: FormEvent<HTMLInputElement> & { data: string }) =>
         /\d/.test(e.data) === false && e.preventDefault();
 
-    return <Input {...props} pattern="\d+" inputMode="numeric" onBeforeInput={validateCharacter} />;
+    return (
+        <Input
+            {...props}
+            type="number"
+            pattern="\d+"
+            inputMode="numeric"
+            onBeforeInput={validateCharacter}
+        />
+    );
 };
 
 type NumberInput = FunctionComponent<
-    Omit<InputProps, "pattern" | "inputMode" | "onBeforeInput" | "value" | "onChange"> & {
+    Omit<InputProps, "pattern" | "inputMode" | "onBeforeInput" | "value" | "onChange" | "type"> & {
         value?: number;
         onChange(value: number): void;
     }
