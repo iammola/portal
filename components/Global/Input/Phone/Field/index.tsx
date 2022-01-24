2f7109baabf5c88d7889ad6aaa88eadae07ddad5
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-24 21:49:49 +0100

    added input and active country select

--- components/Global/Input/Phone/Field/index.tsx
+++ components/Global/Input/Phone/Field/index.tsx
@@ -1,7 +1,31 @@
-import { FunctionComponent } from "react";
+import { ChevronUpIcon } from "@heroicons/react/solid";
+import { FunctionComponent, useState } from "react";
 
-const Field: Field = ({ className }) => {
-    return <div className={className}></div>;
+import Input from "components/Global/Input";
+
+const Field: Field = () => {
+    const [value, setValue] = useState("7400 123456");
+
+    return (
+        <div className="flex flex-row gap-x-3 items-stretch justify-start rounded-xl overflow-hidden bg-white border">
+            <div className="flex flex-row gap-x-0.5 items-center justify-center px-3.5 py-3 bg-slate-100 hover:bg-slate-200">
+                <span className="text-xl">🇬🇧</span>
+                <ChevronUpIcon className="w-6 h-6 fill-slate-600" />
+            </div>
+            <div className="flex flex-row grow gap-x-2 items-center justify-start">
+                <span className="text-lg text-slate-500 font-medium tracking-wide">+44</span>
+                <div className="flex items-center grow w-[12.5rem] h-full">
+                    <Input
+                        required // overflow-hidden hides the optional text
+                        type="tel"
+                        value={value}
+                        onChange={(tel) => setValue(tel as string)}
+                        className="text-lg text-slate-600 font-semibold grow w-full h-[3.75rem] !px-0 !py-3.5 tracking-wide"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 type Field = FunctionComponent<{
