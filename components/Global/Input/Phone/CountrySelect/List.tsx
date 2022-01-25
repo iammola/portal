6f6986fd5c99952bcf1bf05da8f45b2bebf9534b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-25 21:39:37 +0100

    added `selected` props to `List` and `Item` to render `CheckIcon`

--- components/Global/Input/Phone/CountrySelect/List.tsx
+++ components/Global/Input/Phone/CountrySelect/List.tsx
@@ -1,16 +1,18 @@
 import PhoneNumber from "awesome-phonenumber";
 import { byIso } from "country-code-lookup";
 import { FunctionComponent } from "react";
+import { CheckIcon } from "@heroicons/react/solid";
 
 import { useCountryFlag } from "hooks";
 
-const List: List = ({ className }) => {
+const List: List = ({ className, selectedRegion }) => {
     return (
         <ul className={className}>
             {PhoneNumber.getSupportedRegionCodes().map((regionCode) => (
                 <List.Item
                     key={regionCode}
                     regionCode={regionCode}
+                    selected={regionCode === selectedRegion}
                     className="flex flex-row gap-x-4 items-center justify-start p-2 rounded-xl hover:bg-slate-100"
                 />
             ))}
@@ -18,7 +20,7 @@ const List: List = ({ className }) => {
     );
 };
 
-List.Item = function Item({ regionCode, className }) {
+List.Item = function Item({ regionCode, className, selected }) {
     const country = byIso(regionCode)?.country;
     const countryFlag = useCountryFlag(regionCode);
     const otherRegions = {
@@ -37,13 +39,15 @@ List.Item = function Item({ regionCode, className }) {
                 {country ?? otherRegions[regionCode as keyof typeof otherRegions]} (+
                 {PhoneNumber.getCountryCodeForRegionCode(regionCode)})
             </span>
+            {selected === true && <CheckIcon className="w-5 h-5 text-slate-800 ml-auto mr-2" />}
         </li>
     );
 };
 
-interface List extends FunctionComponent<{ className: string }> {
+interface List extends FunctionComponent<{ className: string; selectedRegion: string }> {
     Item: FunctionComponent<{
         className: string;
+        selected: boolean;
         regionCode: string;
     }>;
 }
