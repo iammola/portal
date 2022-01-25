009c7004d6730ca13cee803a639600d7be0af087
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-25 02:17:07 +0100

    added module typings for `if-emoji`

--- types/modules/index.d.ts
+++ types/modules/index.d.ts
@@ -2,3 +2,7 @@ declare module "mongoose-lean-virtuals" {
     import { Schema } from "mongoose";
     export default function mongooseLeanVirtuals(schema: Schema): void;
 }
+
+declare module "if-emoji" {
+    export default function ifEmoji(emoji: string): boolean;
+}
