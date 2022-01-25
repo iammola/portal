dc0c326b8dc74ebac563d1856b3f78aacb2c32d3
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-26 00:22:51 +0100

    added `List` component

--- components/Global/Input/Phone/CountrySelect/index.tsx
+++ components/Global/Input/Phone/CountrySelect/index.tsx
@@ -1,8 +1,15 @@
 import { FunctionComponent } from "react";
 
-const CountrySelect: CountrySelect = () => {
+import List from "./List";
+
+const CountrySelect: CountrySelect = ({ selectedRegion }) => {
     return (
-        <div className="flex flex-col items-start justify-start w-full h-[13.5rem] mt-2 px-2 absolute top-full left-0 rounded-xl shadow-lg overflow-hidden bg-white border border-slate-200 divide-y divide-slate-200"></div>
+        <div className="flex flex-col items-start justify-start w-full h-[13.5rem] mt-2 px-2 absolute top-full left-0 rounded-xl shadow-lg overflow-hidden bg-white border border-slate-200 divide-y divide-slate-200">
+            <List
+                selectedRegion={selectedRegion}
+                className="grow w-full py-2 space-y-1 overflow-x-hidden overflow-y-auto"
+            />
+        </div>
     );
 };
 
