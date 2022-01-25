06567103e4234a0ec9d06fde011f247b84d8878e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-25 12:16:50 +0100

    initial commit of CountrySelect component

---
+++ components/Global/Input/Phone/Field/CountrySelect.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+const CountrySelect: CountrySelect = () => {
+    return <></>;
+};
+
+type CountrySelect = FunctionComponent;
+
+export default CountrySelect;
