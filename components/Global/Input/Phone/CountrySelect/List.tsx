addcde50007c0fd77ffeded561d3063a798af26c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-25 22:45:57 +0100

    made `className` prop a function with `selected` arg

--- components/Global/Input/Phone/CountrySelect/List.tsx
+++ components/Global/Input/Phone/CountrySelect/List.tsx
@@ -4,6 +4,7 @@ import PhoneNumber from "awesome-phonenumber";
 import { byIso } from "country-code-lookup";
 
 import { useCountryFlag } from "hooks";
+import { classNames } from "utils";
 
 const List: List = ({ className, selectedRegion }) => {
     return (
@@ -13,7 +14,15 @@ const List: List = ({ className, selectedRegion }) => {
                     key={regionCode}
                     regionCode={regionCode}
                     selected={regionCode === selectedRegion}
-                    className="flex flex-row gap-x-4 items-center justify-start p-2 rounded-xl hover:bg-slate-100"
+                    className={(selected) =>
+                        classNames(
+                            "flex flex-row gap-x-4 items-center justify-start p-2 rounded-xl",
+                            {
+                                "hover:bg-slate-100": selected === false,
+                                "bg-slate-100 hover:bg-slate-200": selected === true,
+                            }
+                        )
+                    }
                 />
             ))}
         </ul>
@@ -38,7 +47,7 @@ List.Item = function Item({ regionCode, className, selected }) {
         console.warn(`No country data for ${regionCode} region`);
 
     return (
-        <li ref={ref} className={className}>
+        <li ref={ref} className={className(selected)}>
             {countryFlag}
             <span className="text-sm text-slate-700 font-medium">
                 {country ?? otherRegions[regionCode as keyof typeof otherRegions]} (+
@@ -51,9 +60,9 @@ List.Item = function Item({ regionCode, className, selected }) {
 
 interface List extends FunctionComponent<{ className: string; selectedRegion: string }> {
     Item: FunctionComponent<{
-        className: string;
         selected: boolean;
         regionCode: string;
+        className: (selected: boolean) => string;
     }>;
 }
 
