dccdda65ee3913ba46612cae2854739e5de2e2fa
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-25 02:24:02 +0100

    added `Input.Phone` component

--- pages/test.tsx
+++ pages/test.tsx
@@ -6,7 +6,7 @@ import Input from "components/Global/Input";
 import type { NextPage } from "next";
 
 const Test: NextPage = () => {
-    const [value, setValue] = useState<Date>();
+    const [value, setValue] = useState("");
 
     return (
         <main className="flex flex-row items-center justify-center w-screen h-screen bg-slate-200 dark:bg-slate-800">
@@ -14,18 +14,12 @@ const Test: NextPage = () => {
                 <title>Test</title>
             </Head>
             <form className="rounded-lg overflow-hidden bg-white p-8">
-                <Input.Date className="space-y-5">
-                    <Input.Date.Label className="font-semibold text-slate-600 tracking-wide">
-                        Date of Birth
-                    </Input.Date.Label>
-                    <Input.Date.Field
-                        value={value}
-                        onChange={setValue}
-                        min={new Date(2004, 11, 10)}
-                        max={new Date(2014, 11, 10)}
-                        className="flex flex-row gap-x-4 items-center justify-start"
-                    />
-                </Input.Date>
+                <Input.Phone className="space-y-1">
+                    <Input.Phone.Label className="font-medium text-slate-500">
+                        Phone Number
+                    </Input.Phone.Label>
+                    <Input.Phone.Field value={value} regionCode="GB" onChange={setValue} />
+                </Input.Phone>
             </form>
         </main>
     );
