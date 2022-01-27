db1677e665a4590b39b058626f7607d461c951fe
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-27 13:25:54 +0100

    feat(phoneInput): add Icon and Input field :rocket:

--- components/Global/Input/Phone/RegionSelect/Search.tsx
+++ components/Global/Input/Phone/RegionSelect/Search.tsx
@@ -1,7 +1,18 @@
+import { SearchIcon } from "@heroicons/react/outline";
 import { FunctionComponent } from "react";
 
-const Search: Search = () => {
-    return <></>;
+const Search: Search = ({ className, handleSearch, search }) => {
+    return (
+        <div className={className}>
+            <SearchIcon className="w-6 h-6 stroke-slate-700" />
+            <input
+                value={search}
+                placeholder="Search"
+                onChange={(e) => handleSearch(e.target.value)}
+                className="grow w-full p-2 text-slate-700 font-medium focus:outline-none"
+            />
+        </div>
+    );
 };
 
 type Search = FunctionComponent<{
