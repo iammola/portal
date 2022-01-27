f5970fcd57c2486bc160643748cdadc962ed398b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-27 18:16:36 +0100

    feat(hooks): add `useTraceUpdate` to watch deps :hammer:

--- hooks/index.tsx
+++ hooks/index.tsx
@@ -1,7 +1,7 @@
 import ifEmoji from "if-emoji";
 import Flags from "country-flag-icons/react/3x2";
 import getUnicodeFlagIcon from "country-flag-icons/unicode";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useIsChanging<V>(value: V) {
     const [changing, setChanging] = useState(false);
@@ -33,4 +33,25 @@ export function useCountryFlag(regionCode: string) {
     }, [regionCode]);
 
     return countryFlag;
+}
+
+/**
+ * @see https://stackoverflow.com/a/51082563/15350139
+ * @param dependencies - Values you want to track
+ */
+export function useTraceUpdate(dependencies: Record<string, unknown>) {
+    const prev = useRef(dependencies);
+
+    useEffect(() => {
+        const changed = Object.entries(dependencies).reduce(
+            (a, b) =>
+                Object.assign(
+                    a,
+                    prev.current[b[0]] !== b[1] && { [b[0]]: [prev.current[b[0]], b[1]] }
+                ),
+            {}
+        );
+        if (Object.keys(changed).length > 0) console.warn("Changed Deps:", changed);
+        prev.current = dependencies;
+    });
 }
