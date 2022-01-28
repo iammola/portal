752ed6b5623c21642e8f84d32f5ac479c605657c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-28 23:22:15 +0100

    dev(Input): set valid state with validators :computer:

--- components/Global/Input/Password/index.tsx
+++ components/Global/Input/Password/index.tsx
@@ -2,13 +2,30 @@ import { FunctionComponent, useState } from "react";
 
 import Input, { InputProps } from "components/Global/Input";
 
-const Password: Password = ({ id, className, label, ...props }) => {
-  const [valid] = useState<boolean>();
+const Password: Password = ({
+  id,
+  className,
+  label,
+  onChange,
+  validators,
+  ...props
+}) => {
+  const [valid, setValid] = useState<boolean>();
+
+  function handleChange(val: string) {
+    onChange(val);
+    setValid(
+      val === ""
+        ? undefined
+        : validators?.every(({ regex }) => regex.test(val)) ?? true
+    );
+  }
 
   return (
     <Input
       {...props}
       type="password"
+      onChange={handleChange}
       {...({ id, label } as { [key: string]: string })}
       className={typeof className === "string" ? className : className(valid)}
     />
