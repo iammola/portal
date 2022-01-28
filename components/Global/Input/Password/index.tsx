0b6d670947f9b747cc596036d0c378cb184dadba
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-28 22:35:10 +0100

    dev(Input): pass props to password input :computer:

--- components/Global/Input/Password/index.tsx
+++ components/Global/Input/Password/index.tsx
@@ -1,9 +1,15 @@
 import { FunctionComponent } from "react";
 
-import { InputProps } from "components/Global/Input";
+import Input, { InputProps } from "components/Global/Input";
 
-const Password: Password = () => {
-  return <></>;
+const Password: Password = ({ id, label, ...props }) => {
+  return (
+    <Input
+      {...props}
+      type="password"
+      {...({ id, label } as { [key: string]: string })}
+    />
+  );
 };
 
 type Password = FunctionComponent<
