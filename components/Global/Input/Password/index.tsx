1f8a4dc4920999bc79637c07cad2e28bf97ad73b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-28 22:45:19 +0100

    dev(Input): add valid state :computer:

--- components/Global/Input/Password/index.tsx
+++ components/Global/Input/Password/index.tsx
@@ -1,13 +1,16 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 
 import Input, { InputProps } from "components/Global/Input";
 
-const Password: Password = ({ id, label, ...props }) => {
+const Password: Password = ({ id, className, label, ...props }) => {
+  const [valid] = useState<boolean>();
+
   return (
     <Input
       {...props}
       type="password"
       {...({ id, label } as { [key: string]: string })}
+      className={typeof className === "string" ? className : className(valid)}
     />
   );
 };
