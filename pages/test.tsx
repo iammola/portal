10a57ac6268b9dd7b1af32d8aadf0d977ee337c1
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-29 22:33:22 +0100

    feat(Input): import password input

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Head from "next/head";
 
 import Input from "components/Global/Input";
+import { classNames } from "utils";
 
 import type { NextPage } from "next";
 
@@ -14,16 +15,37 @@ const Test: NextPage = () => {
         <title>Test</title>
       </Head>
       <form className="rounded-lg bg-white p-8">
-        <Input.Phone className="w-[22.5rem] space-y-1">
-          <Input.Phone.Label className="font-medium text-slate-500">
-            Phone Number
-          </Input.Phone.Label>
-          <Input.Phone.Field
+        <div className="w-[22.5rem]">
+          <Input.Password
+            required
             value={value}
-            regionCode="GB"
+            id="password"
+            label="Password"
             onChange={setValue}
+            className={(valid?: boolean) =>
+              classNames(
+                "h-[3.75rem] w-[22.5rem] overflow-hidden rounded-lg border placeholder-transparent ring-2 [-webkit-appearance:none] placeholder-shown:border-slate-300 placeholder-shown:ring-transparent focus:border-transparent focus:outline-none focus:ring-blue-400 focus:valid:border-transparent focus:invalid:border-transparent",
+                {
+                  "valid:ring-emerald-400 focus:valid:ring-emerald-400":
+                    valid === true,
+                  "invalid:ring-red-400 focus:invalid:ring-red-400":
+                    valid === false,
+                }
+              )
+            }
+            validators={[
+              {
+                regex: /.{8}/,
+                message: "At least 8 characters",
+              },
+              {
+                regex: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).+/,
+                message:
+                  "Must include a number, a lowercase and uppercase character",
+              },
+            ]}
           />
-        </Input.Phone>
+        </div>
       </form>
     </main>
   );
