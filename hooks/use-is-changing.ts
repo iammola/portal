266f2194308b542f27505753ad680b09dd00b6f5
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-30 16:45:50 +0100

    feathooks): add delay argument to is-changing hook :computer:

--- hooks/use-is-changing.ts
+++ hooks/use-is-changing.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
-export function useIsChanging<V>(value: V) {
+export function useIsChanging<V>(value: V, delay = 5e2) {
   const [changing, setChanging] = useState(false);
 
   useEffect(() => {
     setChanging(true);
-    const timeout = setTimeout(setChanging, 750, false);
+    const timeout = setTimeout(setChanging, delay, false);
     return () => clearTimeout(timeout);
-  }, [value]);
+  }, [delay, value]);
 
   return changing;
 }
