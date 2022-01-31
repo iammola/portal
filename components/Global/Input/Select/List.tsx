9cc87ac09c452034beb613c1da2a626ef4bcb817
Ademola Adedeji <a.mola.dev@gmail.com>
2022-01-31 13:09:41 +0100

    dev(Select): add List and Option props :hammer_and_wrench:

--- components/Global/Input/Select/List.tsx
+++ components/Global/Input/Select/List.tsx
@@ -1,12 +1,19 @@
 import { FunctionComponent } from "react";
 
-export const List: List = () => {
-  return <></>;
+export const List: List = ({ children, className }) => {
+  return <ul className={className}>{children}</ul>;
 };
 
-export const Option: Option = () => {
-  return <></>;
+export const Option: Option = ({ children, className, selected }) => {
+  return (
+    <li className={className(selected)}>
+      <span className="text-sm font-medium text-slate-700">{children}</span>
+    </li>
+  );
 };
 
-export type Option = FunctionComponent;
-export type List = FunctionComponent;
+export type Option = FunctionComponent<{
+  selected: boolean;
+  className: (selected: boolean) => string;
+}>;
+export type List = FunctionComponent<{ className: string }>;
--- components/Global/Input/Select/index.tsx
+++ components/Global/Input/Select/index.tsx
@@ -1,22 +1,58 @@
 import { FunctionComponent, useState } from "react";
 
+import { classNames } from "utils";
+
 import Button from "./Button";
 import { Option, List } from "./List";
 
-const Select: Select = ({ label }) => {
+const Select: Select = ({ label, options, value }) => {
   const [open, setOpen] = useState(false);
-  const [value] = useState<unknown>();
 
   return (
     <div className="relative">
       <Button {...{ open, setOpen }} valueSelected={value != undefined}>
         {label}
       </Button>
+      <List
+        className={classNames(
+          "absolute top-full left-0 z-[1000] mt-2 max-h-[13.5rem] w-full space-y-1 overflow-hidden overflow-y-auto overflow-x-hidden rounded-xl border border-slate-200 bg-white py-2 px-2 shadow-lg",
+          { "pointer-events-none invisible opacity-0": open === false }
+        )}
+      >
+        {options.map((option) => (
+          <Option
+            key={String(option.id)}
+            selected={value === option}
+            className={(selected) =>
+              classNames(
+                "flex cursor-pointer flex-row items-center justify-start gap-x-4 rounded-xl p-2 hover:bg-slate-100 focus:outline-none",
+                {
+                  "focus:bg-slate-50": selected,
+                }
+              )
+            }
+          >
+            {option.value}
+          </Option>
+        ))}
+      </List>
     </div>
   );
 };
 
-interface Select extends FunctionComponent<{ label: string }> {
+interface Value {
+  id: unknown;
+  value: string;
+}
+
+interface SelectProps {
+  value?: Value;
+  label: string;
+  options: Value[];
+  onChange(value: Value): void;
+}
+
+interface Select extends FunctionComponent<SelectProps> {
   List: List;
   Button: Button;
   Option: Option;
