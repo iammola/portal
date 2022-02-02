49bcb3f1356ec979708f4b2d778d6dd6942161f5
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-02 01:11:58 +0100

    feat(Input): add input and preview element :computer:
    kjbn

--- components/Global/Input/UserPortrait/index.tsx
+++ components/Global/Input/UserPortrait/index.tsx
@@ -1,7 +1,65 @@
-import { FunctionComponent } from "react";
+import Image from "next/image";
+import { ChangeEvent, FunctionComponent, useEffect, useState } from "react";
 
-const UserPortrait: UserPortrait = () => {
-  return <></>;
+import Placeholder from "./Placeholder";
+
+const UserPortrait: UserPortrait = ({ onChange, value }) => {
+  const [src, setSrc] = useState("");
+  const [fileName, setFileName] = useState<string>();
+  const [unoptimized, setUnoptimized] = useState(false);
+
+  useEffect(() => {
+    if (typeof value === "string") {
+      setSrc(value);
+      setFileName("");
+      setUnoptimized(false);
+    }
+  }, [value]);
+
+  function getDataURI({ target }: ProgressEvent<FileReader>) {
+    setUnoptimized(true);
+    setSrc((target?.result as string) ?? "");
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+
+    if (file != undefined) {
+      const reader = new FileReader();
+      reader.addEventListener("load", getDataURI);
+
+      onChange(file);
+      setFileName(file.name);
+      reader.readAsDataURL(file);
+    }
+  }
+
+  return (
+    <div className="flex w-[25rem] flex-row items-center justify-start gap-x-2">
+      <div className="relative aspect-square h-[6.5rem] w-[6.5rem] shrink-0 rounded-full bg-slate-300 dark:bg-slate-800">
+        {src === "" ? (
+          <Placeholder className="w-full fill-slate-500 dark:fill-white" />
+        ) : (
+          <Image
+            layout="fill"
+            objectFit="cover"
+            alt="Image Preview"
+            objectPosition="center"
+            className="h-full w-full"
+            {...{ src, unoptimized }}
+          />
+        )}
+      </div>
+      <input
+        type="file"
+        onChange={handleChange}
+        className="w-[113px] text-sm file:cursor-pointer file:rounded-full file:border-none file:bg-blue-200 file:px-4 file:py-2 file:font-medium file:tracking-wide file:text-blue-700 hover:file:bg-blue-300"
+      />
+      <span className="truncate text-sm font-medium tracking-wide text-slate-500">
+        {fileName ?? "no file selected"}
+      </span>
+    </div>
+  );
 };
 
 type UserPortrait = FunctionComponent<{
