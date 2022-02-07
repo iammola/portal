0ab6afed4050be8f543a75ab338925e7256a8879
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-07 21:09:35 +0100

    chore: reset dev test page :hammer_and_wrench:

---
+++ pages/dev.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+import type { NextPage } from "next";
+
+const Dev: NextPage = () => {
+  return (
+    <main className="flex h-screen w-screen flex-row items-center justify-center bg-slate-200 font-inter dark:bg-slate-800">
+      <Head>
+        <title>Development Page</title>
+        <meta name="description" content="Test Components" />
+      </Head>
+    </main>
+  );
+};
+
+export default Dev;
