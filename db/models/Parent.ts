17035f4769b59fe29e5469d6a0776385ad07ae92
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-08 09:34:42 +0100

    initial(models): create parent model file :tada:

---
+++ db/models/Parent.ts
@@ -0,0 +1,46 @@
+import { Schema, model, models } from "mongoose";
+
+import {
+  userDOB,
+  userName,
+  UserImage,
+  userGender,
+  userContact,
+  UserPassword,
+  userSchoolMail,
+} from "db/schema/User";
+
+import type {
+  ParentRecord,
+  ParentModel as ParentModelType,
+} from "types/schema";
+
+const ParentSchema = new Schema<ParentRecord, ParentModelType>({
+  gender: userGender(),
+  schoolMail: userSchoolMail(),
+  dob: userDOB({ required: [true, "Parent DOB required"] }),
+  password: {
+    type: UserPassword,
+    required: [true, "Parent Password required"],
+  },
+  image: {
+    type: UserImage,
+    default: undefined,
+  },
+  name: {
+    type: userName(),
+    required: [true, "Parent name required"],
+  },
+  contact: {
+    type: userContact(),
+    required: [true, "Parent Contact required"],
+  },
+  occupation: {
+    type: String,
+    trim: true,
+  },
+});
+
+export const ParentModel =
+  (models.Parent as ParentModelType) ??
+  model<ParentRecord, ParentModelType>("Parent", ParentSchema);
