d8d29e4db8b2b5eae14486e909bcea8430833862
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-09 10:02:42 +0100

    feat(api): add GET function for classes :electric_plug:

--- types/api/classes/index.ts
+++ types/api/classes/index.ts
@@ -1,7 +1,11 @@
 import type { ClassSchema } from "types/schema";
+import type { FlattenIntersection } from "types/utils";
 
 export type CreateClassData = Pick<ClassSchema, "_id" | "createdAt">;
 export type CreateClassRequestBody = Pick<
   ClassSchema,
   "name" | "alias" | "special" | "teachers"
 >;
+
+export type GetClassesData<S extends keyof ClassSchema = keyof ClassSchema> =
+  FlattenIntersection<Pick<ClassSchema, "_id" | S>>[];
