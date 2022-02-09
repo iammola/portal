fad9f25a0a895c553cb646f0cd033e2b8a3dc553
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-09 11:33:56 +0100

    feat: add Subjects component :sparkles:

--- components/Form/Academic/Components.tsx
+++ components/Form/Academic/Components.tsx
@@ -27,7 +27,53 @@ export const Class: Class = ({ onChange, value }) => {
   );
 };
 
+export const Subjects: Subjects = ({ onChange, selectedClass, values }) => {
+  const [options, setOptions] = useState<Value[]>([]);
+  // NOTE: Type and API Route not yet implemented
+  const { data: subjects } = useSWR<ApiResult<GetClassesData<"name">>>(
+    `/api/classes/${selectedClass}/subjects/?field=name`
+  );
+
+  useEffect(() => {
+    if (subjects?.success)
+      setOptions(subjects.data.map((d) => ({ id: d._id, value: d.name })));
+  }, [subjects]);
+
+  return (
+    <ul>
+      {options.map(({ id, value }) => (
+        <li
+          key={id as string}
+          className="flex flex-row items-center justify-start gap-x-2"
+        >
+          <label htmlFor={`subjectID${id as string}`}>
+            <input
+              type="checkbox"
+              id={`subjectID${id as string}`}
+              checked={values.includes(id as string)}
+              onChange={(e) =>
+                onChange(
+                  e.target.checked
+                    ? [...values, id as string]
+                    : values.filter((i) => i !== id)
+                )
+              }
+            />
+            {value}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 type Class = FunctionComponent<{
   value: string;
   onChange(v: string): void;
+}>;
+
+type Subjects = FunctionComponent<{
+  values: string[];
+  selectedClass: string;
+  onChange(v: string[]): void;
 }>;
