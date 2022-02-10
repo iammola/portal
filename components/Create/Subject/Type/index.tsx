f11509283f715102f2a76232fc8446d496d387ca
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-10 09:15:18 +0100

    feat: add Options to sub-components :sparkles:

--- components/Create/Subject/Type/index.tsx
+++ components/Create/Subject/Type/index.tsx
@@ -1,14 +1,18 @@
 import { FunctionComponent } from "react";
 
+import Options from "./Options";
+
 const SubjectType: SubjectType = ({ children, className }) => {
   return <div className={className}>{children}</div>;
 };
 
+SubjectType.Options = Options;
 SubjectType.Label = ({ children, className }) => {
   return <span className={className}>{children}</span>;
 };
 
 interface SubjectType extends FunctionComponent<{ className: string }> {
+  Options: Options;
   Label: FunctionComponent<{ className: string }>;
 }
 
