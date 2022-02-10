db79957896487d32ed310e5b874fe12b590ff569
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-10 07:26:17 +0100

    dev: add memoized options :construction:

--- components/Create/Subject/Type.tsx
+++ components/Create/Subject/Type.tsx
@@ -1,7 +1,30 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useMemo } from "react";
+
+import { ModelNames } from "db";
+
+import type { SubjectRecord } from "types/schema";
 
 const SubjectType: SubjectType = () => {
+  const options = useMemo<Option[]>(
+    () => [
+      {
+        id: ModelNames.B_SUBJECT,
+        label: "Base",
+      },
+      {
+        id: ModelNames.G_SUBJECT,
+        label: "Grouped",
+      },
+    ],
+    []
+  );
+
   return <></>;
+};
+
+type Option = {
+  label: string;
+  id: SubjectRecord["__type"];
 };
 
 type SubjectType = FunctionComponent<{ label: string }>;
