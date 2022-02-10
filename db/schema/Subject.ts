badb4ecb58ea42e5e6425fcfa92a610310daac9d
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-10 06:21:45 +0100

    refactor(schema): remove schema helper argument repetitions :recycle:

--- db/schema/Subject.ts
+++ db/schema/Subject.ts
@@ -4,49 +4,35 @@ import { ModelNames } from "db";
 
 import type { BaseSubjectRecord, GroupSubjectRecord } from "types/schema";
 
-export const subjectName = (required: string) => ({
+export const subjectName = () => ({
   trim: true,
   unique: true,
   type: String,
-  required: [true, required] as [true, string],
+  required: [true, "Subject name required"] as [true, string],
 });
 
-export const subjectAlias = (
-  required: string,
-  minLength: string,
-  maxLength: string
-) => ({
+export const subjectAlias = () => ({
   trim: true,
   unique: true,
   type: String,
-  minLength: [3, minLength] as const,
-  maxLength: [5, maxLength] as const,
-  required: [true, required] as [true, string],
-});
-
-export const subjectTeachers = () => ({
-  default: undefined,
-  ref: ModelNames.TEACHER,
-  type: [Schema.Types.ObjectId],
+  minLength: [3, "Subject alias min-length = 3"] as const,
+  maxLength: [5, "Subject alias max-length = 5"] as const,
+  required: [true, "Subject alias required"] as [true, string],
 });
 
 export const BaseSubjectSchema = new Schema<BaseSubjectRecord>({
-  name: subjectName("Subject name required"),
-  alias: subjectAlias(
-    "Subject alias required",
-    "Subject alias min-length = 3",
-    "Subject alias max-length = 5"
-  ),
-  teachers: subjectTeachers(),
+  teachers: {
+    default: undefined,
+    ref: ModelNames.TEACHER,
+    type: [Schema.Types.ObjectId],
+  },
+  name: subjectName(),
+  alias: subjectAlias(),
 });
 
 export const GroupSubjectSchema = new Schema<GroupSubjectRecord>({
-  name: subjectName("Subject name required"),
-  alias: subjectAlias(
-    "Subject alias required",
-    "Subject alias min-length = 3",
-    "Subject alias max-length = 5"
-  ),
+  name: subjectName(),
+  alias: subjectAlias(),
   divisions: {
     default: undefined,
     type: [BaseSubjectSchema],
