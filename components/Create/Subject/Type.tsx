d2209ca6c6966004e57aafa83fd911740d9f2051
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-10 03:03:37 +0100

    initial: create subject type file :tada:

---
+++ components/Create/Subject/Type.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+const SubjectType: SubjectType = () => {
+  return <></>;
+};
+
+type SubjectType = FunctionComponent<{ label: string }>;
+
+export default SubjectType;
