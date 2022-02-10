fdc03937457372fee0e046f4118334b927bb5091
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-10 20:18:26 +0100

    dev: create `Option` component :construction:

--- components/Create/Subject/Type/Options.tsx
+++ components/Create/Subject/Type/Options.tsx
@@ -6,7 +6,7 @@ import { useIsomorphicLayoutEffect } from "hooks";
 import type { SubjectRecord } from "types/schema";
 
 const Options: Options = ({ className, id, onChange, value }) => {
-  const options = useMemo<Option[]>(
+  const options = useMemo<OptionValue[]>(
     () => [
       {
         id: ModelNames.B_SUBJECT,
@@ -23,10 +23,6 @@ const Options: Options = ({ className, id, onChange, value }) => {
     ],
     []
   );
-  const IDs = useMemo(
-    () => options.map(() => id + Math.random().toString(36).slice(2)),
-    [id, options]
-  );
 
   useIsomorphicLayoutEffect(() => {
     if (value === undefined) onChange(options[0].id);
@@ -34,40 +30,68 @@ const Options: Options = ({ className, id, onChange, value }) => {
 
   return (
     <ul className={className}>
-      {options.map((option, i) => (
-        <li key={option.id} className="">
-          <label htmlFor={IDs[i]} className="grid grid-cols-5 grid-rows-4">
-            <input
-              name={id}
-              id={IDs[i]}
-              type="radio"
-              checked={option.id === value}
-              onChange={(e) => e.target.checked && onChange(option.id)}
-            />
-            <h4 className="col-start-1 col-end-5 row-start-1 row-end-2 text-sm font-bold uppercase tracking-wide">
-              {option.label}
-            </h4>
-            <p className="col-start-1 col-end-6 row-start-2 row-end-5 text-sm text-slate-400">
-              {option.description}
-            </p>
-          </label>
-        </li>
+      {options.map((option) => (
+        <Option
+          uniqueId={id}
+          key={option.id}
+          {...{ onChange, ...option }}
+          selected={option.id === value}
+        />
       ))}
     </ul>
   );
 };
 
-export type Option = {
+const Option: Option = ({
+  description,
+  id,
+  label,
+  onChange,
+  selected,
+  uniqueId,
+}) => {
+  const unique = useMemo(
+    () => uniqueId + Math.random().toString(36).slice(2),
+    [uniqueId]
+  );
+
+  return (
+    <li>
+      <label htmlFor={unique} className="grid grid-cols-5 grid-rows-4">
+        <input
+          name={id}
+          id={unique}
+          type="radio"
+          checked={selected}
+          onChange={(e) => e.target.checked && onChange(id)}
+        />
+        <h4 className="col-start-1 col-end-5 row-start-1 row-end-2 text-sm font-bold uppercase tracking-wide">
+          {label}
+        </h4>
+        <p className="col-start-1 col-end-6 row-start-2 row-end-5 text-sm text-slate-400">
+          {description}
+        </p>
+      </label>
+    </li>
+  );
+};
+
+export type OptionValue = {
   label: string;
   description: string;
   id: SubjectRecord["__type"];
 };
 
+type Option = FunctionComponent<
+  OptionValue &
+    Pick<OptionsProps, "onChange"> & { selected: boolean; uniqueId: string }
+>;
+
 type OptionsProps = {
   id: string;
   className: string;
-  value?: Option["id"];
-  onChange(v: Option["id"]): void;
+  value?: OptionValue["id"];
+  onChange(v: OptionValue["id"]): void;
 };
 
 type Options = FunctionComponent<OptionsProps>;
