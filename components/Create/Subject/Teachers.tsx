8aacb6d1d286d3cd9e365e75f22afd7b7b4c2769
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-11 07:11:56 +0100

    feat: allow Teachers Email field height to be customisable :sparkles:

--- components/Create/Subject/Teachers.tsx
+++ components/Create/Subject/Teachers.tsx
@@ -4,13 +4,13 @@ import Email, { Value } from "components/Form/Email";
 
 const Teachers: Teachers = ({ onChange, values = [] }) => {
   return (
-    <Email className="space-y-2">
+    <Email className="flex h-full flex-col space-y-2">
       <Email.Label className="font-medium text-slate-800">Teachers</Email.Label>
       <Email.Field
         values={values}
         userType="teacher"
         onChange={onChange}
-        className="flex h-24 w-full grow flex-row flex-wrap justify-start gap-x-3 gap-y-2 rounded-lg border border-slate-200 bg-white px-3 py-[17px] ring-2 ring-transparent focus:border-transparent focus:outline-none focus:ring-blue-400"
+        className="flex w-full grow flex-row flex-wrap justify-start gap-x-3 gap-y-2 rounded-lg border border-slate-200 bg-white px-3 py-[17px] ring-2 ring-transparent focus:border-transparent focus:outline-none focus:ring-blue-400"
       />
     </Email>
   );
