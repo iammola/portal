82df4051f97336c17efa29d1e2f2cc5f450095c8
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-11 04:30:59 +0100

    feat: add Email field :sparkles:

--- components/Create/Subject/Base/Teachers.tsx
+++ components/Create/Subject/Base/Teachers.tsx
@@ -1,9 +1,24 @@
 import { FunctionComponent } from "react";
 
-const Teachers: Teachers = () => {
-  return <></>;
+import Email, { Value } from "components/Form/Email";
+
+const Teachers: Teachers = ({ onChange, values }) => {
+  return (
+    <Email className="space-y-2">
+      <Email.Label className="font-medium text-slate-800">Teachers</Email.Label>
+      <Email.Field
+        values={values}
+        userType="teacher"
+        onChange={onChange}
+        className="flex h-24 w-full grow flex-row flex-wrap justify-start gap-x-3 gap-y-2 rounded-lg border border-slate-200 bg-white px-3 py-[17px] ring-2 ring-transparent focus:border-transparent focus:outline-none focus:ring-blue-400"
+      />
+    </Email>
+  );
 };
 
-type Teachers = FunctionComponent;
+type Teachers = FunctionComponent<{
+  values?: Value[];
+  onChange(v: Value[]): void;
+};
 
 export default Teachers;
