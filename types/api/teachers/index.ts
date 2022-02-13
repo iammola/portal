d09b88752d8a102b0d03e605249c3bdf82bc8738
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-14 00:34:34 +0100

    dev(types): set password type to string in request body :construction:

--- types/api/teachers/index.ts
+++ types/api/teachers/index.ts
@@ -3,5 +3,5 @@ import type { TeacherSchema } from "types/schema";
 export type CreateTeacherData = Pick<TeacherSchema, "_id" | "schoolMail">;
 export type CreateTeacherRequestBody = Omit<
   TeacherSchema,
-  "_id" | "schoolMail"
->;
+  "_id" | "schoolMail" | "password"
+> & { password: string };
