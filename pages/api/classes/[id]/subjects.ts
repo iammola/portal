c598a34283e52f8369ce2e392a1cf587b9e9c43c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-15 22:54:21 +0100

    dev(api): add Model logic :construction:

--- pages/api/classes/[id]/subjects.ts
+++ pages/api/classes/[id]/subjects.ts
@@ -2,6 +2,7 @@ import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
 import { connect } from "db";
 import { formatApiError } from "utils/api";
+import { BaseSubjectModel, GroupSubjectModel, TeacherModel } from "db/models";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { ApiInternal, ApiInternalResponse } from "types/api";
@@ -14,6 +15,46 @@ type Return = ApiInternalResponse<CreateSubjectData>;
 
 async function createSubject(data: CreateSubjectRequestBody, classID: string) {
   await connect();
+  let [result, statusCode]: ApiInternal<CreateSubjectData> = ["", 0];
+
+  const teachers = await TeacherModel.find(
+    {
+      schoolMail:
+        data.__type === "base"
+          ? data.teachers
+          : data.divisions.map((d) => d.teachers).flat(),
+    },
+    "schoolMail"
+  ).lean();
+
+  const { _id } =
+    data.__type === "base"
+      ? await BaseSubjectModel.create({
+          ...data,
+          class: classID,
+          teachers: teachers.map((t) => t._id),
+        })
+      : await GroupSubjectModel.create({
+          ...data,
+          class: classID,
+          divisions: data.divisions.map((div) => ({
+            ...div,
+            teachers: div.teachers
+              .map((t) => teachers.find((d) => d.schoolMail === t)?._id)
+              .filter(Boolean),
+          })),
+        });
+
+  [result, statusCode] = [
+    {
+      success: true,
+      data: { _id },
+      message: ReasonPhrases.OK,
+    },
+    StatusCodes.OK,
+  ];
+
+  return [result, statusCode] as const;
 }
 
 export default async function handler(
