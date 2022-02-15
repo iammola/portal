dffea6bbdc9574bd646ae2b1db0cf11e01c06ed4
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-15 23:26:04 +0100

    dev(types): omit `_id` from request body :construction_worker:

--- types/api/subjects.ts
+++ types/api/subjects.ts
@@ -2,11 +2,11 @@ import type { BaseSubjectSchema, GroupSubjectSchema } from "types/schema";
 
 export type CreateSubjectData = Pick<BaseSubjectSchema, "_id">;
 export type CreateSubjectRequestBody =
-  | (Omit<GroupSubjectSchema, "divisions"> & {
+  | (Omit<GroupSubjectSchema, "_id" | "divisions"> & {
       divisions: (Pick<BaseSubjectSchema, "_id" | "name" | "alias"> & {
         teachers: string[];
       })[];
     })
-  | (Omit<BaseSubjectSchema, "teachers"> & {
+  | (Omit<BaseSubjectSchema, "_id" | "teachers"> & {
       teachers: string[];
     });
