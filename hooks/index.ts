cd7d0e5e19a3025a278b128686774f6c827eba5a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-17 01:47:16 +0100

    initial(hooks): create `useTabs` hook file :tada:

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,4 +1,5 @@
-export { useIsomorphicLayoutEffect } from "hooks/use-isomorphic-layout-effect";
+export { useTabs } from "hooks/use-tabs";
+export { useIsChanging } from "hooks/use-is-changing";
 export { useCountryFlag } from "hooks/use-country-flag";
 export { useTraceUpdate } from "hooks/use-trace-update";
-export { useIsChanging } from "hooks/use-is-changing";
+export { useIsomorphicLayoutEffect } from "hooks/use-isomorphic-layout-effect";
---
+++ hooks/use-tabs.ts
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export function useTabs<T extends string>(defaultTab: T) {
+  const router = useRouter();
+  const [active, setActive] = useState<T>();
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    const {
+      pathname,
+      query: { tab: current, ...query },
+    } = router;
+    const args = [undefined, { shallow: true }] as const;
+
+    if (active === undefined && current === undefined) setActive(defaultTab);
+    else if (![defaultTab, current].includes(active))
+      void router.push({ pathname, query: { ...query, tab: active } }, ...args);
+    else if (active === defaultTab && current !== undefined)
+      void router.push({ pathname, query }, ...args);
+  }, [active, defaultTab, router]);
+
+  return [active, setActive] as const;
+}
