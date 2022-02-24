630b9546509db0cc1057b05385779768909fb794
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-24 18:32:53 +0100

    dev(utils): add `routeWrapper` function :construction:

--- types/api/index.ts
+++ types/api/index.ts
@@ -1,16 +1,20 @@
-import type { StatusCodes, ReasonPhrases } from "http-status-codes";
 import type { DocumentId } from "types/schema";
 import type { FilterNumber } from "types/utils";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { StatusCodes, ReasonPhrases } from "http-status-codes";
 
 export type CreateResult<O = unknown> = DocumentId & O;
 export type DeleteResult = Record<"success", boolean>;
 export type UpdateResult = Record<"success", boolean>;
 
-export type ApiInternal<D> = ["", 0] | ApiInternalResponse<D>;
-
-export type ApiInternalResponse<D> =
+export type HandlerResponse<D> =
   | [ApiError, FilterNumber<`${StatusCodes}`>]
   | [ApiResponse<D>, FilterNumber<`${StatusCodes}`>];
+
+export type ApiHandler<R extends Record<string, unknown>> = (
+  req: NextApiRequest,
+  res: NextApiResponse<ApiError | ApiResponse<R>>
+) => Promise<HandlerResponse<R> | null>;
 
 export interface ApiData<S> {
   success: S;
--- utils/api/index.ts
+++ utils/api/index.ts
@@ -1,2 +1,42 @@
-export { formatApiError } from "./error";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+import { formatError } from "./error";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { ApiHandler, HandlerResponse } from "types/api";
+
+export async function routeWrapper<T extends Record<string, unknown>>(
+  req: NextApiRequest,
+  res: NextApiResponse<HandlerResponse<T>[0]>,
+  routeHandler: ApiHandler<T>,
+  methods: string[]
+) {
+  let data: HandlerResponse<T> | null = null;
+
+  try {
+    if (methods.includes(req.method ?? "")) data = await routeHandler(req, res);
+  } catch (error) {
+    data = [
+      {
+        success: false,
+        error: formatError(error),
+        message: ReasonPhrases.BAD_REQUEST,
+      },
+      StatusCodes.BAD_REQUEST,
+    ];
+  }
+
+  res
+    .status(data?.[1] ?? StatusCodes.METHOD_NOT_ALLOWED)
+    .setHeader("Allow", methods)
+    .setHeader("Content-Type", "application/json")
+    .json(
+      data?.[0] ?? {
+        success: false,
+        error: ReasonPhrases.METHOD_NOT_ALLOWED,
+        message: ReasonPhrases.METHOD_NOT_ALLOWED,
+      }
+    );
+}
+
 export { fetchAPIEndpoint } from "./endpoint";
