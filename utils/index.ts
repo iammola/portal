f4665fb9508521a5f2a1cc3c6ed148b785ebad41
Ademola Adedeji <a.mola.dev@gmail.com>
2022-02-28 13:10:42 +0100

    refactor(utils): create constants util file :recycle:

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 // can't export from "./file" as the googleapis module causes an error when deploying
 export * from "./email";
 export * from "./password";
+export * from "./constants";
 export * from "./classnames";
--- utils/password.ts
+++ utils/password.ts
@@ -2,9 +2,6 @@ import { pbkdf2Sync, randomBytes } from "crypto";
 
 import type { UserPassword } from "types/schema";
 
-export const JWT_ALG = "PS256";
-export const JWT_COOKIE = "grs-jwt";
-
 const PASSWORD_LENGTH = 64;
 const PASSWORD_ITERATIONS = 1e5;
 const PASSWORD_ENCODING = "hex";
