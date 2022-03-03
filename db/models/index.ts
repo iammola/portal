a5d2fde77a34d10de030b064b1b8e7476abb8426
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-03 23:58:12 +0100

    dev(models): export Term & Session models :construction:

--- db/models/index.ts
+++ db/models/index.ts
@@ -1,6 +1,8 @@
+export { TermModel } from "./Term";
 export { AuthModel } from "./Auth";
 export { ClassModel } from "./Class";
 export { ParentModel } from "./Parent";
 export { TeacherModel } from "./Teacher";
+export { SessionModel } from "./Session";
 export { StudentModel } from "./Student";
 export { BaseSubjectModel, GroupSubjectModel, SubjectModel } from "./Subject";
