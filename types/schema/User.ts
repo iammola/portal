ab9b334cf687d1c07258a5f4654e1bd5e5ee0140
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-04 22:13:09 +0100

    fix(types): use HydratedDocument to get Query :bug:

--- types/schema/User.ts
+++ types/schema/User.ts
@@ -45,11 +45,11 @@ export interface UserVirtuals {
 
 export interface UserStaticMethods<S> {
   /** Find a user by username */
-  findByUsername(username: string, projection?: any): SQuery<S> | null;
+  findByUsername(username: string, projection?: any): SQuery<S>;
   /** Find all users by username */
-  findByUsername(username: string[], projection?: any): SQuery<S[]>;
+  findByUsername(username: string[], projection?: any): SQuery<S[], S>;
   /** Find a user by school mail */
-  findBySchoolMail(mail: string, projection?: any): SQuery<S> | null;
+  findBySchoolMail(mail: string, projection?: any): SQuery<S>;
   /** Find all users by schoolMail */
-  findBySchoolMail(mail: string[], projection?: any): SQuery<S[]>;
+  findBySchoolMail(mail: string[], projection?: any): SQuery<S[], S>;
 }
