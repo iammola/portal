83814b9b7b0d39b935d76ed298db5d28a88661e7
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-05 13:33:27 +0100

    feat(utils): pass Auth header to fetch init :fire:

--- utils/api/endpoint.ts
+++ utils/api/endpoint.ts
@@ -1,4 +1,7 @@
+import { parse } from "cookie";
+
 import { ApiResult } from "types/api";
+import { JWT_COOKIE } from "utils/constants";
 
 /**
  * @param endpoint The API endpoint to fetch
@@ -10,12 +13,15 @@ export async function fetchAPIEndpoint<ResponseData, Body = undefined>(
   init?: Omit<RequestInit, "body">,
   body?: Body
 ) {
+  const JWT = parse(document.cookie)[JWT_COOKIE];
+
   const response = await fetch(endpoint, {
     ...init,
     body: JSON.stringify(body),
     headers: {
       ...init?.headers,
       Accept: "application/json",
+      Authorization: JWT && `Bearer ${JWT}`,
     },
   });
   return (await response.json()) as ApiResult<ResponseData>;
