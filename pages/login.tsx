c24d178573018fd60866efaccb8c604d127fbe30
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-05 18:22:41 +0100

    dev(app): add Head elements 🧱

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,14 @@
-import { NextPage } from "next";
+import Head from "next/head";
+
 import type { NextPage } from "next";
 
 const Login: NextPage = () => {
-  return <></>;
+  return (
+    <main className="flex h-screen w-screen items-stretch justify-center overflow-hidden bg-black font-urbane">
+      <Head>
+        <title>Login | GRS CBT</title>
+      </Head>
+  );
 };
 
 export default Login;
