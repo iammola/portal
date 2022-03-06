cb0f03f39c9174dad3d92d5dc381e317b6c37cba
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-06 12:22:46 +0100

    fix(app): change title color 🎨

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,7 +13,7 @@ const Login: NextPage = () => {
       </Head>
       <figure className="w-[48.5vw] bg-gray-900" />
       <section className="flex grow flex-col items-center justify-center gap-y-[4.5rem] bg-white p-5">
-        <h1 className="text-5xl font-light text-slate-500">Sign in to Portal</h1>
+        <h1 className="text-5xl font-light text-gray-600">Sign in to Portal</h1>
         <div className="flex w-full max-w-lg flex-col items-center justify-center gap-y-6 px-10">
           <form className="w-full space-y-8">
             <div className="flex w-full flex-col items-start justify-center gap-y-1">
