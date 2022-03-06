6d949c8ac9ada72393e990444a84e54efdfd549c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-06 05:49:29 +0100

    dev(app): add title and bg-color ⌨️
    sqas

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,6 +8,13 @@ const Login: NextPage = () => {
       <Head>
         <title>Login | GRS CBT</title>
       </Head>
+      <figure className="w-[48.5vw] bg-gray-900" />
+      <section className="flex grow flex-col items-center justify-center gap-y-[4.5rem] bg-white p-5">
+        <div className="flex w-full max-w-lg flex-col items-center justify-center gap-y-6 px-10">
+          <h1 className="text-5xl font-light text-slate-500">Sign in to Portal</h1>
+        </div>
+      </section>
+    </main>
   );
 };
 
