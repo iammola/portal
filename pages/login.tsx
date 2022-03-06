239d388cfcc35133a04178a5505b2ea354ae2510
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-06 19:50:56 +0100

    feat(app): add level listbox ✨

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,14 +2,24 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import { SelectorIcon } from "@heroicons/react/solid";
+import { Listbox, Transition } from "@headlessui/react";
 
+import { classNames } from "utils";
 import { Input, Password } from "components/Form";
 
 import type { NextPage } from "next";
 
+const levels = [
+  { emoji: "👨‍🎓", value: "student" },
+  { emoji: "👨‍🏫", value: "teacher" },
+  { emoji: "👨‍👩‍👧‍👦", value: "parent" },
+];
+
 const Login: NextPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [level, setLevel] = useState<{ value: string; emoji: string }>();
 
   return (
     <main className="flex h-screen w-screen items-stretch justify-center overflow-hidden bg-black font-urbane">
@@ -39,6 +49,56 @@ const Login: NextPage = () => {
                 className="h-14 w-full rounded-lg border-none bg-slate-50 pl-6 text-slate-700 placeholder-slate-200 focus:outline-none"
               />
             </div>
+            <Listbox
+              as="div"
+              value={level}
+              onChange={setLevel}
+              className="relative flex w-full flex-col items-start justify-center gap-y-1"
+            >
+              <Listbox.Label className="text-sm font-light tracking-wider text-slate-600">
+                Account type
+              </Listbox.Label>
+              <Listbox.Button className="flex h-14 w-full items-center justify-between rounded-lg bg-slate-50 py-2 px-4">
+                {level && (
+                  <span className="grid grid-cols-[max-content_1fr] items-center gap-x-6 text-sm text-slate-700">
+                    <span className="text-xl">{level.emoji}</span>
+                    <span className="capitalize tracking-wide text-slate-700">{level.value}</span>
+                  </span>
+                )}
+                <SelectorIcon className="ml-auto h-5 w-5 fill-slate-600" />
+              </Listbox.Button>
+              <Transition
+                enter="transition duration-100 ease-in"
+                enterFrom="scale-95 opacity-0"
+                enterTo="scale-100 opacity-100"
+                leave="transition duration-100 ease-out"
+                leaveFrom="scale-100 opacity-100"
+                leaveTo="scale-95 opacity-0"
+                className="absolute top-[5.5rem] z-50 max-h-60 w-full bg-white"
+              >
+                <Listbox.Options className="h-full w-full overflow-y-auto rounded-md py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {levels.map((level) => (
+                    <Listbox.Option
+                      value={level}
+                      key={level.value}
+                      className={({ active, selected }) =>
+                        classNames(
+                          "grid cursor-pointer select-none grid-cols-[max-content_1fr] items-center gap-x-6 py-3 px-4",
+                          [selected, "bg-blue-600 text-white", "bg-white text-slate-700"],
+                          {
+                            "bg-blue-500": active && selected,
+                            "bg-blue-100": active && !selected,
+                          }
+                        )
+                      }
+                    >
+                      <span className="text-xl">{level.emoji}</span>
+                      <span className="capitalize tracking-wider">{level.value}</span>
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              </Transition>
+            </Listbox>
             <div className="flex w-full flex-col items-start justify-center gap-y-1">
               <label
                 htmlFor="password"
