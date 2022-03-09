265666c3c011dcc7706e79b8c66bcb444b22cb66
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-09 13:20:51 +0100

    dev(app): add Head & Page Title ⚙️

--- pages/classes/[name].tsx
+++ pages/classes/[name].tsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
+
 import type { NextPage } from "next";
 
 const Class: NextPage = () => {
-  return <></>;
+  return (
+    <main className="flex h-screen w-screen items-stretch justify-center overflow-hidden font-urbane">
+      <Head>
+        <title>Class-Name | **SessionIfNotCurrent** | GRS Portal</title>
+      </Head>
+    </main>
+  );
 };
 
 export default Class;
