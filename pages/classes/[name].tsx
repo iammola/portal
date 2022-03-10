f25f509dc3e8618a11e7c3c6a87893f62f1334c0
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-10 22:28:02 +0100

    feat(page): add tabs 🗂

--- pages/classes/[name].tsx
+++ pages/classes/[name].tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
+import { useState } from "react";
 import { format } from "date-fns";
 
+import { classNames } from "utils";
 import { Breadcrumbs } from "components/Breadcrumbs";
 
 import type { NextPage } from "next";
 
 const Class: NextPage = () => {
+  const [activeTab, setActiveTab] = useState("Feed");
+
   return (
     <main className="flex h-screen w-screen items-stretch justify-center overflow-hidden font-urbane">
       <Head>
@@ -23,6 +27,22 @@ const Class: NextPage = () => {
             <span>3 subjects</span>
           </p>
         </header>
+        <section className="w-full py-6 px-5">
+          <div className="relative flex w-full items-center justify-start gap-x-12 border-b-2 border-slate-300">
+            {["Feed", "Students", "Subjects"].map((tab) => (
+              <span
+                key={tab}
+                onClick={() => setActiveTab(tab)}
+                className={classNames(
+                  "relative cursor-pointer px-2 pb-3 tracking-wide text-slate-500 after:absolute after:-bottom-0.5 after:left-0 after:h-0.5 after:w-full",
+                  { "after:bg-gray-500": tab === activeTab }
+                )}
+              >
+                {tab}
+              </span>
+            ))}
+          </div>
+        </section>
       </section>
     </main>
   );
