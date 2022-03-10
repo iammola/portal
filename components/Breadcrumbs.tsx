1999388ff261c44228d8b9efc995d974ce8de9be
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-10 13:00:58 +0100

    refactor(components): use memo to store paths array 💪

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Fragment, FunctionComponent } from "react";
+import { Fragment, FunctionComponent, useMemo } from "react";
 import { ChevronRightIcon, HomeIcon } from "@heroicons/react/solid";
 
 export const Breadcrumbs: FunctionComponent<Props> = ({ className }) => {
   const router = useRouter();
+  const paths = useMemo(() => router.route.slice(1).split("/"), [router]);
 
   return (
     <nav className={className ?? "flex w-full gap-x-2 py-2"}>
@@ -13,19 +14,16 @@ export const Breadcrumbs: FunctionComponent<Props> = ({ className }) => {
           <HomeIcon className="h-5 w-5 fill-slate-500" />
         </a>
       </Link>
-      {router.route
-        .slice(1)
-        .split("/")
-        .map((page, index, array) => (
-          <Fragment key={page}>
-            <ChevronRightIcon className="h-5 w-5 fill-slate-500" />
-            <Link href={"/" + array.slice(0, index + 1).join("/")}>
-              <a className="font-urbane text-sm capitalize text-slate-500">
-                {page.replaceAll("-", " ")}
-              </a>
-            </Link>
-          </Fragment>
-        ))}
+      {paths.map((page, index, array) => (
+        <Fragment key={page}>
+          <ChevronRightIcon className="h-5 w-5 fill-slate-500" />
+          <Link href={"/" + array.slice(0, index + 1).join("/")}>
+            <a className="font-urbane text-sm capitalize text-slate-500">
+              {page.replaceAll("-", " ")}
+            </a>
+          </Link>
+        </Fragment>
+      ))}
     </nav>
   );
 };
