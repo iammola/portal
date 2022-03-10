f60bf643162f49c9edf8143052bcf06d1bb0f307
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-10 18:47:36 +0100

    dev(page): use breadcrumbs & add class name 👍

--- pages/classes/[name].tsx
+++ pages/classes/[name].tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import { Breadcrumbs } from "components/Breadcrumbs";
+
 import type { NextPage } from "next";
 
 const Class: NextPage = () => {
@@ -8,6 +10,12 @@ const Class: NextPage = () => {
       <Head>
         <title>Class-Name | **SessionIfNotCurrent** | GRS Portal</title>
       </Head>
+      <section className="flex w-full grow flex-col items-start justify-start">
+        <header className="flex w-full flex-col bg-slate-100 px-8 pt-4 pb-6">
+          <Breadcrumbs />
+          <h3 className="text-4xl font-semibold capitalize text-slate-700">Key Stage 1</h3>
+        </header>
+      </section>
     </main>
   );
 };
