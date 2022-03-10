0544d81f195677b0d8f1e930427912cbeba03c1f
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-10 21:47:39 +0100

    feat(page): add class stats 📈

--- pages/classes/[name].tsx
+++ pages/classes/[name].tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { format } from "date-fns";
 
 import { Breadcrumbs } from "components/Breadcrumbs";
 
@@ -14,6 +15,13 @@ const Class: NextPage = () => {
         <header className="flex w-full flex-col bg-slate-100 px-8 pt-4 pb-6">
           <Breadcrumbs />
           <h3 className="text-4xl font-semibold capitalize text-slate-700">Key Stage 1</h3>
+          <p className="flex gap-x-1 pt-2 text-xs font-light tracking-wide text-slate-500">
+            <span>Since {format(new Date(2018, 5, 22), "do MMMM yyyy")}</span>
+            {"•"}
+            <span>5 students</span>
+            {"•"}
+            <span>3 subjects</span>
+          </p>
         </header>
       </section>
     </main>
