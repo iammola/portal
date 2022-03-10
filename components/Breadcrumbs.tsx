900c7c8763757b46b590b6bfb5b181fc0133586a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-10 18:36:22 +0100

    enh(breadcrumbs): reduce color intensity 👍

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -12,14 +12,14 @@ export const Breadcrumbs: FunctionComponent<Props> = ({ className }) => {
     <nav className={className ?? "flex w-full gap-x-2 py-2"}>
       <Link href="/">
         <a>
-          <HomeIcon className="h-5 w-5 fill-slate-500" />
+          <HomeIcon className="h-5 w-5 fill-slate-400" />
         </a>
       </Link>
       {paths.map((page, index, array) => (
         <Fragment key={page}>
-          <ChevronRightIcon className="h-5 w-5 fill-slate-500" />
+          <ChevronRightIcon className="h-5 w-5 fill-slate-400" />
           <Link href={"/" + array.slice(0, index + 1).join("/")}>
-            <a className="font-urbane text-sm capitalize text-slate-500">
+            <a className="font-urbane text-sm capitalize text-slate-400">
               {page.replaceAll("-", " ")}
             </a>
           </Link>
