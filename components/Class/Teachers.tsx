db25b8826fdd6643e4da7b74c21c9f97661b8286
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-16 18:50:49 +0100

    feat(component): add API request to get teachers 👍

--- components/Class/Teachers.tsx
+++ components/Class/Teachers.tsx
@@ -1,3 +1,4 @@
+import useSWR from "swr";
 import Link from "next/link";
 import { FunctionComponent } from "react";
 import {
@@ -9,43 +10,23 @@ import {
 
 import { UserImage } from "components";
 
-const teachers = [
-  // Todo: Use this API route to get actual data
-  // Route - `/api/classes/${props._id.toString()}/teachers?filter=name schoolMail`
-  // Type - import type { GetClassTeachersData } from "types/api/classes";
-  {
-    _id: "1",
-    image: {
-      portrait: "/Users/002.jpg",
-    },
-    schoolMail: "seth.ade@mola.io",
-    name: {
-      full: "Seth Adedeji",
-      initials: "SA",
-    },
-  },
-  {
-    _id: "2",
-    image: {
-      portrait: "/Users/006.jpg",
-    },
-    schoolMail: "noah.ade@mola.io",
-    name: {
-      full: "Noah Adedeji",
-      initials: "NA",
-    },
-  },
-];
+import type { ApiResponse } from "types/api";
+import type { TeacherSchema } from "types/schema";
+import type { GetClassTeachersData as Data } from "types/api/classes";
 
-export const Teachers: FunctionComponent<{ id: string }> = () => {
+export const Teachers: FunctionComponent<{ id: string }> = ({ id }) => {
+  const { data: res } = useSWR<ApiResponse<Data>>(
+    `/api/classes/${id}/teachers?projection=schoolMail,name.full,name.initials,name.title,image.portrait`
+  );
+
   return (
     <div className="flex flex-col items-end justify-center gap-y-4 py-5">
       <button className="rounded bg-blue-500 px-4 py-2 text-sm tracking-wide text-white shadow hover:bg-blue-600">
         Invite Teacher
       </button>
       <div className="w-full divide-y divide-slate-300">
-        {teachers?.map((item) => (
-          <Row key={item._id} {...item} />
+        {res?.data.teachers.map((item) => (
+          <Row key={String(item._id)} {...item} />
         ))}
         <div className="flex items-center justify-between text-xs">
           <div className="flex cursor-pointer items-center gap-x-3 py-4 text-slate-600">
@@ -72,7 +53,7 @@ export const Teachers: FunctionComponent<{ id: string }> = () => {
   );
 };
 
-const Row: FunctionComponent<typeof teachers[0]> = ({ _id, image, name, schoolMail }) => {
+const Row: FunctionComponent<TeacherSchema> = ({ _id, image, name, schoolMail }) => {
   return (
     <div
       className="grid w-full gap-x-28 py-5"
@@ -90,7 +71,7 @@ const Row: FunctionComponent<typeof teachers[0]> = ({ _id, image, name, schoolMa
           />
         </div>
         <div className="space-y-1 text-sm">
-          <Link href={`/teachers/${_id}`}>
+          <Link href={`/teachers/${String(_id)}`}>
             <a className="tracking-wide text-blue-600">{name.full}</a>
           </Link>
           <div className="flex gap-x-2">
@@ -104,7 +85,7 @@ const Row: FunctionComponent<typeof teachers[0]> = ({ _id, image, name, schoolMa
           </div>
         </div>
       </div>
-      <Link href={`/teachers/${_id}`}>
+      <Link href={`/teachers/${String(_id)}`}>
         <a className="flex items-center text-sm tracking-wide text-blue-600">
           Go to Profile
           <ExternalLinkIcon className="h-4 w-4 fill-blue-600" />
