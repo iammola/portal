cb62717765e1d99c7264ccb73055c331b596ea53
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-17 11:49:31 +0100

    dev(List): add `className` prop 💻

--- components/List.tsx
+++ components/List.tsx
@@ -1,5 +1,9 @@
 import { FunctionComponent } from "react";
 
-export const List: FunctionComponent = () => {
-  return <></>;
+export const List: FunctionComponent<Props> = ({ children, className }) => {
+  return <div className={className}>{children}</div>;
 };
+
+interface Props {
+  className: string;
+}
