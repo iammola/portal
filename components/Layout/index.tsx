c4f8df4c6ddadbdbffe715d9dfc0ce94d16c80d7
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-26 20:06:24 +0100

    feat(layout): add prop to hide sidebar ✨

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -3,11 +3,16 @@ import { FunctionComponent } from "react";
 import { Body } from "./Body";
 import { Sidebar } from "./Sidebar";
 
-export const Layout: FunctionComponent = ({ children }) => {
+export const Layout: FunctionComponent<LayoutProps> = ({ children, hideSidebar }) => {
   return (
     <main className="flex h-screen w-screen items-stretch overflow-hidden font-urbane">
-      <Sidebar />
+      {!hideSidebar && <Sidebar />}
       <Body>{children}</Body>
     </main>
   );
 };
+
+interface LayoutProps {
+  /** Hide sidebar in `/login` route */
+  hideSidebar: boolean;
+}
