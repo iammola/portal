bb2b94bb961cf2a75f511c90ea65a02bb6a40903
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-26 18:04:19 +0100

    initial(sidebar): create Footer component 🎉

---
+++ components/Layout/Sidebar/Footer.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from "react";
+
+export const Footer: FunctionComponent = () => {
+  return <div className="flex items-center justify-between gap-x-4 p-4" />;
+};
--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -1,11 +1,13 @@
 import { FunctionComponent } from "react";
 
 import { Menu } from "./Menu";
+import { Footer } from "./Footer";
 
 export const Sidebar: FunctionComponent = () => {
   return (
     <aside className="h-full w-[22.5rem] divide-y divide-slate-300 overflow-y-auto">
       <Menu />
+      <Footer />
     </aside>
   );
 };
