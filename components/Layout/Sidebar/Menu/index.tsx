924a265fa79e4b079c2dd4893a13b259a5165cd0
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-26 17:28:17 +0100

    feat(sidebar): add collapsible menu item ✨

--- components/Layout/Sidebar/Menu/index.tsx
+++ components/Layout/Sidebar/Menu/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FunctionComponent } from "react";
+import { ChevronRightIcon } from "@heroicons/react/solid";
 
 export const Menu: FunctionComponent = () => {
   return (
@@ -10,6 +11,30 @@ export const Menu: FunctionComponent = () => {
           <div className="grow truncate text-sm font-light tracking-wide text-gray-700">Attendance Report</div>
         </a>
       </Link>
+      <details className="group">
+        <summary className="flex h-11 w-full cursor-pointer items-center justify-start gap-x-4 rounded-lg px-4 hover:bg-slate-100">
+          <ChevronRightIcon className="h-5 w-5 shrink-0 fill-gray-700 group-open:rotate-90" />
+          <div className="grow truncate text-sm font-light tracking-wide text-gray-700">People</div>
+        </summary>
+        <Link href="">
+          <a className="flex h-11 w-full cursor-pointer items-center justify-start gap-x-4 rounded-lg pl-8 pr-4 hover:bg-slate-100">
+            <span className="flex h-5 w-5 shrink-0 items-center justify-center after:h-1.5 after:w-1.5 after:rounded-full after:ring-2 after:ring-gray-700" />
+            <div className="grow truncate text-sm font-light tracking-wide text-gray-700">Students</div>
+          </a>
+        </Link>
+        <Link href="">
+          <a className="flex h-11 w-full cursor-pointer items-center justify-start gap-x-4 rounded-lg pl-8 pr-4 hover:bg-slate-100">
+            <span className="flex h-5 w-5 shrink-0 items-center justify-center after:h-1.5 after:w-1.5 after:rounded-full after:ring-2 after:ring-gray-700" />
+            <div className="grow truncate text-sm font-light tracking-wide text-gray-700">Teachers</div>
+          </a>
+        </Link>
+        <Link href="">
+          <a className="flex h-11 w-full cursor-pointer items-center justify-start gap-x-4 rounded-lg pl-8 pr-4 hover:bg-slate-100">
+            <span className="flex h-5 w-5 shrink-0 items-center justify-center after:h-1.5 after:w-1.5 after:rounded-full after:ring-2 after:ring-gray-700" />
+            <div className="grow truncate text-sm font-light tracking-wide text-gray-700">Parents</div>
+          </a>
+        </Link>
+      </details>
     </div>
   );
 };
