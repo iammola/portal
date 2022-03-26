0b8b2409402ee06a566c992c3d0d4aafa988a761
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-26 18:17:28 +0100

    feat(sidebar): add footer content ✨

--- components/Layout/Sidebar/Footer.tsx
+++ components/Layout/Sidebar/Footer.tsx
@@ -1,5 +1,21 @@
 import { FunctionComponent } from "react";
 
+import { UserImage } from "components";
+
 export const Footer: FunctionComponent = () => {
-  return <div className="flex items-center justify-between gap-x-4 p-4" />;
+  return (
+    <div className="flex items-center gap-x-2 p-4">
+      <div className="aspect-square h-12 w-12 overflow-hidden rounded-full shadow">
+        <UserImage
+          fallbackText="RP"
+          src="/Users/005.jpg"
+          fallbackClassName="bg-slate-300 tracking-widest text-sm text-gray-600"
+        />
+      </div>
+      <div>
+        <div className="truncate text-sm text-slate-700">Rebecca Pearson</div>
+        <div className="truncate text-xs tracking-wide text-slate-500">rebecca.pearson@thisisus.com</div>
+      </div>
+    </div>
+  );
 };
