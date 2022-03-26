f9a776a0885103a54e3d33953052dafc47679d97
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-26 16:29:35 +0100

    feat(sidebar): add Menu component 👍

---
+++ components/Layout/Sidebar/Menu/index.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from "react";
+
+export const Menu: FunctionComponent = () => {
+  return <div className="space-y-2 p-4" />;
+};
--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -1,5 +1,11 @@
 import { FunctionComponent } from "react";
 
+import { Menu } from "./Menu";
+
 export const Sidebar: FunctionComponent = () => {
-  return <aside className="h-full w-[22.5rem] overflow-y-auto" />;
+  return (
+    <aside className="h-full w-[22.5rem] divide-y divide-slate-300 overflow-y-auto">
+      <Menu />
+    </aside>
+  );
 };
