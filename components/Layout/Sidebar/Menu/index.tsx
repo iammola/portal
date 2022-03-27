8a7086919c0e39d6d3fb9344b01a1c405971a23f
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-27 18:12:45 +0100

    feat(sidebar): build sidebar from user nav JSON 👍

--- components/Layout/Sidebar/Menu/index.tsx
+++ components/Layout/Sidebar/Menu/index.tsx
@@ -1,18 +1,35 @@
-import { FunctionComponent } from "react";
+import { parse } from "cookie";
+import { Fragment, FunctionComponent, useMemo } from "react";
+
+import { USER_COOKIE } from "utils";
 
 import { Item, List } from "./Items";
+import navigation from "./navigation.json";
 
 export const Menu: FunctionComponent = () => {
-  const items = [
-    { href: "", title: "Students" },
-    { href: "", title: "Teachers" },
-    { href: "", title: "Parents" },
-  ];
+  const user = parse(document.cookie)[USER_COOKIE] as "student" | "teacher" | "parent";
+  const items = useMemo(() => navigation[user] as unknown as Navigation[], [user]);
 
   return (
     <div className="w-full grow space-y-2 p-4">
-      <Item href="">Attendance Report</Item>
-      <List items={items}>People</List>
+      {items.map(({ href, items, title }) => (
+        <Fragment key={title}>
+          {href !== undefined && <Item href={href}>{title}</Item>}
+          {items !== undefined && <List items={items}>{title}</List>}
+        </Fragment>
+      ))}
     </div>
   );
 };
+
+interface NavigationItem {
+  href: string;
+  title: string;
+}
+
+interface NavigationList {
+  title: string;
+  items: NavigationItem[];
+}
+
+type Navigation = (NavigationList & Partial<NavigationItem>) | (NavigationItem & Partial<NavigationList>);
