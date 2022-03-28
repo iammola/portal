1f9175e29d35806e80a037c62d631e8d11ae6a6e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-28 20:22:49 +0100

    feat(hooks): use number to store active tabs 👏
    - Easier to use with HeadlessUI's Tabs component
    - Reset the URL if the tab query isn't a valid tab

--- hooks/use-tabs.ts
+++ hooks/use-tabs.ts
@@ -3,27 +3,36 @@ import { useRouter } from "next/router";
 
 import { useIsomorphicLayoutEffect } from "hooks";
 
-export function useTabs<T extends string>(defaultTab: T) {
+export function useTabs<T extends string>(tabs: T[], defaultIndex = 0) {
   const router = useRouter();
-  const [active, setActive] = useState<T>();
+  const [active, setActive] = useState(-1);
 
   useIsomorphicLayoutEffect(() => {
     if (!router.isReady) return;
 
     const {
       pathname,
-      query: { tab: current, ...query },
+      query: { tab, ...query },
     } = router;
 
-    if (active === undefined) return setActive((current as T) ?? defaultTab);
+    if (active === -1) {
+      const idx = tabs.indexOf(tab as T);
+      return setActive(~idx ? idx : defaultIndex);
+    }
 
+    const currentIndex = getCurrentIndex();
     const args = [undefined, { shallow: true }] as const;
 
-    if (![defaultTab, current].includes(active))
-      void router.push({ pathname, query: { ...query, tab: active } }, ...args);
-    else if (current !== undefined && [active, current].includes(defaultTab))
+    if (![defaultIndex, currentIndex].includes(active))
+      void router.push({ pathname, query: { ...query, tab: tabs[active] } }, ...args);
+    else if (currentIndex !== undefined && [active, currentIndex].includes(defaultIndex))
       void router.replace({ pathname, query }, ...args);
-  }, [active, defaultTab, router]);
+
+    function getCurrentIndex() {
+      const idx = tabs.indexOf(tab as T);
+      return ~idx ? idx : undefined;
+    }
+  }, [active, defaultIndex, router, tabs]);
 
   return [active, setActive] as const;
 }
