5e560ca98aca59f09887d2670a9c01cca6d4c06a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-28 16:27:07 +0100

    fix(middleware): allow auth API requests ⚒️

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,8 +4,9 @@ import { JWT_COOKIE_TOKEN } from "utils/constants";
 
 export const middleware: NextMiddleware = (req) => {
   const key = req.cookies[JWT_COOKIE_TOKEN];
+  const authRoutes = ["/login", "/api/auth"];
 
-  if (req.nextUrl.pathname !== "/login" && !key) {
+  if (!authRoutes.includes(req.nextUrl.pathname) && !key) {
     const url = req.nextUrl.clone();
     url.pathname = "/login";
 
