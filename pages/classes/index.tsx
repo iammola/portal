488cca70391cf40e0d93dcd2af11e7b6d1d9fe9d
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-28 17:15:23 +0100

    initial(classes): create classes page 🎉

---
+++ pages/classes/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+import type { NextPage } from "next";
+
+const Classes: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Classes | GRS Portal</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default Classes;
