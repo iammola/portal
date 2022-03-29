915db06d376a725c3ec4e919d981f5b1171d66dd
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-29 07:19:28 +0100

    feat(body): use flex to stack children ✨

--- components/Layout/Body.tsx
+++ components/Layout/Body.tsx
@@ -1,5 +1,9 @@
 import { FunctionComponent } from "react";
 
 export const Body: FunctionComponent = ({ children }) => {
-  return <section className="h-full grow overflow-y-auto bg-slate-100">{children}</section>;
+  return (
+    <section className="flex h-full grow flex-col items-start justify-start overflow-y-auto bg-slate-100">
+      {children}
+    </section>
+  );
 };
