58f904a16c69180080778172c27e4ca7d25b649a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-29 12:45:46 +0100

    feat(sidebar): add Tab components and state 💪

--- components/Layout/Sidebar/Menu/Items.tsx
+++ components/Layout/Sidebar/Menu/Items.tsx
@@ -1,3 +1,4 @@
+import { Tab } from "@headlessui/react";
 import { FunctionComponent } from "react";
 import Link, { LinkProps } from "next/link";
 import { ChevronRightIcon } from "@heroicons/react/solid";
@@ -6,27 +7,39 @@ import { classNames } from "utils";
 
 export const Item: FunctionComponent<ItemProps> = ({ children, className, href }) => {
   return (
-    <Link href={href}>
-      <a
-        className={classNames(
-          "flex h-11 w-full cursor-pointer items-center justify-start gap-x-4 rounded-lg px-4 hover:bg-slate-50",
-          className
-        )}
-      >
-        <span className="relative flex h-5 w-5 shrink-0 items-center justify-center after:h-1.5 after:w-1.5 after:rounded-full after:bg-white after:ring-2 after:ring-gray-700" />
-        <div className="grow truncate text-sm font-light tracking-wide text-gray-700">{children}</div>
-      </a>
-    </Link>
+    <Tab
+      className={({ selected }) =>
+        classNames("h-11 w-full cursor-pointer rounded-lg hover:bg-slate-50", { "bg-slate-50": selected }, className)
+      }
+    >
+      <Link href={href}>
+        <a className="flex h-full items-center justify-start gap-x-4 px-4">
+          <span className="relative flex h-5 w-5 shrink-0 items-center justify-center after:h-1.5 after:w-1.5 after:rounded-full after:bg-white after:ring-2 after:ring-gray-700" />
+          <div className="grow truncate text-left text-sm font-light tracking-wide text-gray-700">{children}</div>
+        </a>
+      </Link>
+    </Tab>
   );
 };
 
 export const List: FunctionComponent<ListProps> = ({ children, items }) => {
   return (
-    <details className="group">
-      <summary className="flex h-11 w-full cursor-pointer items-center justify-start gap-x-4 rounded-lg px-4 hover:bg-slate-50 group-open:mb-1 group-open:bg-slate-50">
+    <Tab.List
+      as="details"
+      className="group"
+    >
+      <Tab
+        as="summary"
+        className={({ selected }) =>
+          classNames(
+            "flex h-11 w-full cursor-pointer items-center justify-start gap-x-4 rounded-lg px-4 hover:bg-slate-50 group-open:mb-1 group-open:bg-slate-50",
+            { "bg-slate-50": selected }
+          )
+        }
+      >
         <ChevronRightIcon className="h-5 w-5 shrink-0 fill-gray-700 group-open:rotate-90" />
         <div className="grow truncate text-sm font-light tracking-wide text-gray-700">{children}</div>
-      </summary>
+      </Tab>
       {items.map((item) => (
         <Item
           key={item.title}
@@ -36,7 +49,7 @@ export const List: FunctionComponent<ListProps> = ({ children, items }) => {
           {item.title}
         </Item>
       ))}
-    </details>
+    </Tab.List>
   );
 };
 
--- components/Layout/Sidebar/Menu/index.tsx
+++ components/Layout/Sidebar/Menu/index.tsx
@@ -1,5 +1,7 @@
 import { parse } from "cookie";
-import { Fragment, FunctionComponent, useState } from "react";
+import { Tab } from "@headlessui/react";
+import { useRouter } from "next/router";
+import { Fragment, FunctionComponent, useCallback, useState } from "react";
 
 import { USER_COOKIE } from "utils";
 import { useIsomorphicLayoutEffect } from "hooks";
@@ -8,22 +10,41 @@ import { Item, List } from "./Items";
 import navigation from "./navigation.json";
 
 export const Menu: FunctionComponent = () => {
+  const router = useRouter();
   const [items, setItems] = useState<Navigation[]>([]);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const onChange = useCallback(() => {
+    if (!router.isReady || items.length === 0) return;
+
+    const paths = ["/", ...items.map((i) => i.href ?? (i.items ?? []).map((i) => i.href))];
+    const idx = paths.findIndex((p) => p === router.pathname || p.indexOf(router.pathname) > -1);
+
+    setSelectedIndex(~idx ? idx : 0);
+  }, [items, router]);
 
   useIsomorphicLayoutEffect(() => {
     const user = parse(document.cookie)[USER_COOKIE] as "student" | "teacher" | "parent";
     setItems(navigation[user] as unknown as Navigation[]);
   }, []);
 
+  useIsomorphicLayoutEffect(onChange, [items, onChange, router]);
+
   return (
-    <div className="w-full grow space-y-2 p-4">
-      {items.map(({ href, items, title }) => (
-        <Fragment key={title}>
-          {href !== undefined && <Item href={href}>{title}</Item>}
-          {items !== undefined && <List items={items}>{title}</List>}
-        </Fragment>
-      ))}
-    </div>
+    <Tab.Group
+      manual
+      vertical
+      {...{ onChange, selectedIndex }}
+    >
+      <Tab.List className="w-full grow space-y-2 p-4">
+        {items.map(({ href, items, title }) => (
+          <Fragment key={title}>
+            {href !== undefined && <Item href={href}>{title}</Item>}
+            {items !== undefined && <List items={items}>{title}</List>}
+          </Fragment>
+        ))}
+      </Tab.List>
+    </Tab.Group>
   );
 };
 
