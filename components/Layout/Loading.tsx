da2be948c4135ae3320c2a406d26a8ce053f5752
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-31 00:14:01 +0100

    feat(layout): add loading progress bar 🎨 (#147)

---
+++ components/Layout/Loading.tsx
@@ -0,0 +1,28 @@
+import { VoidFunctionComponent } from "react";
+import { useNProgress } from "@tanem/react-nprogress";
+
+import { classNames } from "utils";
+
+export const Loading: VoidFunctionComponent<{ isAnimating: boolean }> = (props) => {
+  const { animationDuration, isFinished, progress } = useNProgress(props);
+
+  return (
+    <div
+      style={{ transitionDuration: `${animationDuration}ms` }}
+      className={classNames("pointer-events-none transition-opacity", { "opacity-0": isFinished })}
+    >
+      <div
+        style={{
+          marginLeft: `${(-1 + progress) * 100}%`,
+          transitionDuration: `${animationDuration}ms`,
+        }}
+        className="fixed left-0 top-0 z-50 h-[4px] w-full rounded-r-full bg-[#de1738]  transition-[margin-left]"
+      >
+        <div
+          style={{ boxShadow: "0 0 10px #de1738, 0 0 5px #de1738" }}
+          className="absolute right-0 block h-full w-[100px] translate-x-0 translate-y-[-4px] rotate-3"
+        />
+      </div>
+    </div>
+  );
+};
--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,11 +1,39 @@
-import { FunctionComponent } from "react";
+import { useRouter } from "next/router";
+import { FunctionComponent, useEffect, useState } from "react";
 
 import { Body } from "./Body";
+import { Loading } from "./Loading";
 import { Sidebar } from "./Sidebar";
 
 export const Layout: FunctionComponent<LayoutProps> = ({ children, hideSidebar }) => {
+  const router = useRouter();
+  const [key, setKey] = useState(0);
+  const [routeChanging, setRouteChanging] = useState(false);
+
+  useEffect(() => {
+    const handleStop = () => setRouteChanging(false);
+    const handleStart = () => {
+      setRouteChanging(true);
+      setKey((key) => key ^ 1);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router.events]);
+
   return (
     <main className="flex h-screen w-screen items-stretch overflow-hidden font-urbane">
+      <Loading
+        key={key}
+        isAnimating={routeChanging}
+      />
       {!hideSidebar && <Sidebar />}
       <Body>{children}</Body>
     </main>
