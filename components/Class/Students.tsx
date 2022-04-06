d460fd4b0dd99fe2640801399eb41e54b628bd45
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-06 13:48:37 +0100

    dev(students): add List component ⌨️

--- components/Class/Students.tsx
+++ components/Class/Students.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from "react";
 
+import { List } from "components";
+
 export const Students: FunctionComponent = () => {
-  return <></>;
+  return <List className="w-full divide-y divide-slate-300" />;
 };
