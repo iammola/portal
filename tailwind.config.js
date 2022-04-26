ceb6226991f5290d135080bd4f88c90d76a25312
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-26 13:28:21 +0100

    feat: use radix-ui colors as pallete 🎨 (#163)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require("@radix-ui/colors");
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
@@ -5,6 +6,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["pages/**/*.{ts,tsx}", "components/**/*.{ts,tsx}", "hooks/**/*.{ts,tsx}"],
   theme: {
+    colors: radixToTailwindConfig(),
     extend: {
       fontFamily: {
         inter: ["Inter", ...fontFamily.sans],
@@ -16,3 +18,36 @@ module.exports = {
   variants: {},
   plugins: [require("@tailwindcss/forms")],
 };
+
+/**
+ * @param {string} step
+ * @param {string} color
+ */
+function getStep(step, color) {
+  return step.replace(new RegExp(`${color}a?`, "i"), "");
+}
+
+function radixToTailwindConfig() {
+  const initial = {
+    white: "white",
+    black: "black",
+    current: "currentColor",
+    transparent: "transparent",
+  };
+
+  return Object.entries(colors).reduce((acc, [color, colorSteps]) => {
+    const arr = color.split(/(?=[A-Z])/);
+    const steps = Object.entries(colorSteps).reduce(
+      (acc, [step, value]) => ({
+        ...acc,
+        [getStep(step, arr[0])]: value,
+      }),
+      {}
+    );
+
+    return {
+      ...acc,
+      [arr.join("-").toLowerCase()]: steps,
+    };
+  }, initial);
+}
