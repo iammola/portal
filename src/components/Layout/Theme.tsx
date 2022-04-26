7a1ecce73d3bf5f5447de2689cf70710e67ffa77
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-26 17:23:45 +0100

    feat(theme-picker): add active theme tooltip ✨

--- src/components/Layout/Theme.tsx
+++ src/components/Layout/Theme.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useTheme } from "next-themes";
+import * as Tooltip from "@radix-ui/react-tooltip";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 export const ThemePicker: React.FC = () => {
@@ -9,13 +10,27 @@ export const ThemePicker: React.FC = () => {
   );
 
   return (
-    <button
-      type="button"
-      onClick={() => setTheme(toggle[theme ?? "dark"])}
-      className="fixed bottom-8 right-8 z-[9999] grid place-items-center rounded-full bg-gray-3 p-4 text-gray-12 ring-1 ring-gray-7 hover:bg-gray-4 hover:ring-gray-8 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:ring-gray-dark-7 dark:hover:bg-gray-dark-4 dark:hover:ring-gray-dark-8"
-    >
-      {theme === "dark" && <MoonIcon className="h-12 w-12" />}
-      {theme === "light" && <SunIcon className="h-12 w-12" />}
-    </button>
+    <Tooltip.Root delayDuration={100}>
+      <Tooltip.Trigger asChild>
+        <button
+          type="button"
+          onClick={() => setTheme(toggle[theme ?? "dark"])}
+          className="fixed bottom-8 right-8 z-[9999] grid place-items-center rounded-full bg-gray-3 p-4 text-gray-12 ring-1 ring-gray-7 hover:bg-gray-4 hover:ring-gray-8 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:ring-gray-dark-7 dark:hover:bg-gray-dark-4 dark:hover:ring-gray-dark-8"
+        >
+          {theme === "dark" && <MoonIcon className="h-12 w-12" />}
+          {theme === "light" && <SunIcon className="h-12 w-12" />}
+        </button>
+      </Tooltip.Trigger>
+      <Tooltip.Content
+        asChild
+        side="top"
+        align="center"
+        sideOffset={5}
+      >
+        <span className="rounded-md bg-white px-4 py-2.5 text-sm tracking-wide text-gray-12">
+          <span className="capitalize">{theme}</span> theme active
+        </span>
+      </Tooltip.Content>
+    </Tooltip.Root>
   );
 };
