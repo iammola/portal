96ebb97493adcacad3ad47c24da1ce974f1483ee
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-27 15:53:37 +0100

    feat(avatar): add component styles 🎨

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,11 +3,18 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
 export const Avatar: React.FC = () => {
   return (
-    <AvatarPrimitive.Root>
+    <AvatarPrimitive.Root className="relative inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full bg-black-a-3 align-middle dark:bg-white-a-3">
       <AvatarPrimitive.Image asChild>
-        <Image src="" />
+        <Image
+          src=""
+          layout="fill"
+          className="h-full w-full rounded-[inherit] object-cover"
+        />
       </AvatarPrimitive.Image>
-      <AvatarPrimitive.Fallback delayMs={600} />
+      <AvatarPrimitive.Fallback
+        delayMs={600}
+        className="grid h-full w-full place-items-center bg-white text-sm font-medium text-black-a-11 dark:bg-black dark:text-white-a-11"
+      />
     </AvatarPrimitive.Root>
   );
 };
