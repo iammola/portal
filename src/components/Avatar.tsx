1e4b7bf38d00b037ef08830c67cdc0921cd34ba7
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-27 16:00:24 +0100

    feat(avatar): add props and content 💪

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,20 +1,29 @@
 import Image from "next/image";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
-export const Avatar: React.FC = () => {
+export const Avatar: React.FC<AvatarProps> = (props) => {
   return (
     <AvatarPrimitive.Root className="relative inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full bg-black-a-3 align-middle dark:bg-white-a-3">
       <AvatarPrimitive.Image asChild>
         <Image
-          src=""
           layout="fill"
+          src={props.src}
+          alt={props.name}
           className="h-full w-full rounded-[inherit] object-cover"
         />
       </AvatarPrimitive.Image>
       <AvatarPrimitive.Fallback
         delayMs={600}
         className="grid h-full w-full place-items-center bg-white text-sm font-medium text-black-a-11 dark:bg-black dark:text-white-a-11"
-      />
+      >
+        {props.initials}
+      </AvatarPrimitive.Fallback>
     </AvatarPrimitive.Root>
   );
 };
+
+interface AvatarProps extends CP {
+  src: string;
+  name: string;
+  initials: string;
+}
