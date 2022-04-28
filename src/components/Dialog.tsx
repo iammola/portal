8553e429bac8117fae939e02d3a7fa0976df13b0
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-28 23:54:18 +0100

    dev(dialog): use `React.forwardRef` for `DialogContent` 🖥

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 
@@ -9,12 +10,13 @@ export const DialogTitle = DialogPrimitive.Title;
 
 export const DialogDescription = DialogPrimitive.Description;
 
-export const DialogContent: React.FC<CP<DialogPrimitive.DialogContentProps>> = ({ children, ...contentProps }) => {
-  return (
+export const DialogContent = forwardRef<HTMLDivElement, CP<DialogPrimitive.DialogContentProps>>(
+  ({ children, ...contentProps }, forwardedRef) => (
     <DialogPrimitive.Portal>
       <DialogPrimitive.Overlay className="fixed inset-0 bg-black-a-9" />
       <DialogPrimitive.Content
         {...contentProps}
+        ref={forwardedRef}
         className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-6 sm:max-w-lg"
       >
         {children}
@@ -26,5 +28,7 @@ export const DialogContent: React.FC<CP<DialogPrimitive.DialogContentProps>> = (
         </DialogPrimitive.Close>
       </DialogPrimitive.Content>
     </DialogPrimitive.Portal>
-  );
-};
+  )
+);
+
+DialogContent.displayName = "DialogContent";
