b7fa4cb947aa46336dd654c789fffb06b6fbafd1
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-28 04:04:13 +0100

    feat(toast): add `Error` and `Success` kinds 👍

--- src/components/Layout/Toast.tsx
+++ src/components/Layout/Toast.tsx
@@ -45,6 +45,8 @@ export const ToastProvider: React.FC<ToastPrimitive.ToastProviderProps> = ({ chi
       <ToastPrimitive.Provider {...providerProps}>
         {toasts.map(({ kind, ...toast }) => {
           if (kind === "loading") return <LoadingToast {...toast} />;
+          if (kind === "success") return <SuccessToast {...toast} />;
+          if (kind === "error") return <ErrorToast {...toast} />;
         })}
         <ToastPrimitive.Viewport className="fixed bottom-0 right-0 z-[99999999] flex w-[390px] max-w-[100vw] flex-col gap-2.5 p-6" />
       </ToastPrimitive.Provider>
@@ -71,6 +73,68 @@ const LoadingToast: React.FC<Toast.LoadingProps> = ({ description, ...toastProps
           className="h-4 w-4 text-gray-11 dark:text-gray-dark-11"
         />
       </ToastPrimitive.Close>
+    </ToastPrimitive.Root>
+  );
+};
+
+const SuccessToast: React.FC<Toast.SuccessProps> = ({ action, description, ...toastProps }) => {
+  return (
+    <ToastPrimitive.Root
+      {...toastProps}
+      className="flex w-full items-center justify-start gap-2 rounded-md bg-white py-2.5 pl-4 pr-2 shadow-sm shadow-green-a-9 ring-1 ring-green-6 dark:bg-gray-dark-3 dark:ring-green-dark-6"
+    >
+      <ToastPrimitive.Description className="grow text-sm font-medium tracking-wide text-gray-12 dark:text-gray-dark-12">
+        {description}
+      </ToastPrimitive.Description>
+      {action ? (
+        <ToastPrimitive.Action
+          {...action}
+          className="inline-flex items-center justify-center rounded bg-green-2 py-1 px-2.5 text-xs font-medium text-green-11 ring-1 ring-green-7 hover:ring-green-8 dark:bg-green-dark-2 dark:text-green-dark-11 dark:ring-green-dark-7 dark:hover:ring-green-dark-8"
+        >
+          {action.text}
+        </ToastPrimitive.Action>
+      ) : (
+        <ToastPrimitive.Close
+          aria-label="Close"
+          className="rounded-full p-1.5 hover:bg-gray-4 active:bg-gray-5 dark:hover:bg-gray-dark-4 dark:active:bg-gray-dark-5"
+        >
+          <Cross2Icon
+            aria-hidden
+            className="h-4 w-4 text-gray-11 dark:text-gray-dark-11"
+          />
+        </ToastPrimitive.Close>
+      )}
+    </ToastPrimitive.Root>
+  );
+};
+
+const ErrorToast: React.FC<Toast.ErrorProps> = ({ action, description, ...toastProps }) => {
+  return (
+    <ToastPrimitive.Root
+      {...toastProps}
+      className="flex w-full items-center justify-start gap-2 rounded-md bg-white py-2.5 pl-4 pr-2 shadow-sm shadow-red-a-9 ring-1 ring-red-6 dark:bg-gray-dark-3 dark:ring-red-dark-6"
+    >
+      <ToastPrimitive.Description className="grow text-sm font-medium tracking-wide text-gray-12 dark:text-gray-dark-12">
+        {description}
+      </ToastPrimitive.Description>
+      {action ? (
+        <ToastPrimitive.Action
+          {...action}
+          className="inline-flex items-center justify-center rounded bg-red-2 py-1 px-2.5 text-xs font-medium text-red-11 ring-1 ring-red-7 hover:ring-red-8 dark:bg-red-dark-2 dark:text-red-dark-11 dark:ring-red-dark-7 dark:hover:ring-red-dark-8"
+        >
+          {action.text}
+        </ToastPrimitive.Action>
+      ) : (
+        <ToastPrimitive.Close
+          aria-label="Close"
+          className="rounded-full p-1.5 hover:bg-gray-4 active:bg-gray-5 dark:hover:bg-gray-dark-4 dark:active:bg-gray-dark-5"
+        >
+          <Cross2Icon
+            aria-hidden
+            className="h-4 w-4 text-gray-11 dark:text-gray-dark-11"
+          />
+        </ToastPrimitive.Close>
+      )}
     </ToastPrimitive.Root>
   );
 };
