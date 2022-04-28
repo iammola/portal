f0174607d3d4f767a04f08759d0d8cd47c67f6eb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-29 00:42:55 +0100

    feat: alert dialog component ✨ (#172)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * as Icons from "./Icons";
 export { Avatar } from "./Avatar";
 export * as Dialog from "./Dialog";
+export * as AlertDialog from "./Alert";
 
 export { Layout, useToast } from "./Layout";
