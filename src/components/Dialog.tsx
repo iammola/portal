a783164cb976d9f86d8cf7ad7eb9227fe458b220
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-28 23:38:31 +0100

    dev(dialog): export dialog primitives 🖥

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,3 +1,9 @@
-export const Dialog: React.FC = () => {
-  return <></>;
-};
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+
+export const Dialog = DialogPrimitive.Root;
+
+export const DialogTrigger = DialogPrimitive.Trigger;
+
+export const DialogTitle = DialogPrimitive.Title;
+
+export const DialogDescription = DialogPrimitive.Description;
