6aa880de91beb56f9c48a673af53b2f3a80d9bde
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-28 23:40:41 +0100

    feat(dialog): add `Close` button to `DialogContent` 👏

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,3 +1,4 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 
 export const Dialog = DialogPrimitive.Root;
@@ -17,6 +18,12 @@ export const DialogContent: React.FC<CP<DialogPrimitive.DialogContentProps>> = (
         className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-6 sm:max-w-lg"
       >
         {children}
+        <DialogPrimitive.Close
+          aria-label="Close"
+          className="absolute top-2.5 right-2.5 inline-flex items-center justify-center rounded-full p-2 text-gray-11 hover:bg-gray-4 dark:hover:bg-gray-dark-4"
+        >
+          <Cross2Icon className="h-4 w-4" />
+        </DialogPrimitive.Close>
       </DialogPrimitive.Content>
     </DialogPrimitive.Portal>
   );
