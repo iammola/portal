f6db1888847d26e347c2b84716fb7f251bc1e9ac
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-29 21:08:32 +0100

    feat(layout): add `useOnline` hook ✨

--- src/components/Layout/Body.tsx
+++ src/components/Layout/Body.tsx
@@ -1,3 +1,7 @@
+import { useOnline } from "utils";
+
 export const Body: React.FC<CP> = ({ children }) => {
+  useOnline();
+
   return <section className="bg-slate-100 flex h-full grow flex-col justify-start overflow-y-auto">{children}</section>;
 };
