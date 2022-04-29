33f71968ab2e69a438272d4563013a8a9a6b52ce
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-29 02:55:36 +0100

    enh: `AlertDialog` and `Dialog` component styles 🎨 (#176)

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,13 +4,47 @@ export const AlertDialog = AlertDialogPrimitive.Root;
 
 export const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 
-export const AlertDialogTitle = AlertDialogPrimitive.Title;
+export const AlertDialogAction: React.FC<AlertDialogActionProps> = ({ cancelProps, children, ...props }) => {
+  return (
+    <div className="flex w-full justify-end gap-6">
+      <AlertDialogPrimitive.Cancel
+        {...cancelProps}
+        className="inline-flex items-center justify-center rounded bg-gray-3 px-4 py-2.5 text-sm font-medium text-gray-11 hover:bg-gray-4 active:bg-gray-5 dark:bg-gray-dark-3 dark:hover:bg-gray-dark-4 dark:active:bg-gray-dark-5"
+      >
+        Cancel
+      </AlertDialogPrimitive.Cancel>
+      <AlertDialogPrimitive.Action {...props}>{children}</AlertDialogPrimitive.Action>
+    </div>
+  );
+};
 
-export const AlertDialogDescription = AlertDialogPrimitive.Description;
+export const AlertDialogTitle: React.FC<CP<AlertDialogPrimitive.AlertDialogTitleProps, string>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <AlertDialogPrimitive.Title
+      {...props}
+      className="text-lg font-medium text-gray-12"
+    >
+      {children}
+    </AlertDialogPrimitive.Title>
+  );
+};
 
-export const AlertDialogAction = AlertDialogPrimitive.Action;
-
-export const AlertDialogCancel = AlertDialogPrimitive.Cancel;
+export const AlertDialogDescription: React.FC<CP<AlertDialogPrimitive.AlertDialogDescriptionProps, string>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <AlertDialogPrimitive.Description
+      {...props}
+      className="mb-5 text-sm text-gray-11 dark:text-gray-dark-11"
+    >
+      {children}
+    </AlertDialogPrimitive.Description>
+  );
+};
 
 export const AlertDialogContent: React.FC<CP<AlertDialogPrimitive.AlertDialogContentProps>> = ({
   children,
@@ -28,3 +62,8 @@ export const AlertDialogContent: React.FC<CP<AlertDialogPrimitive.AlertDialogCon
     </AlertDialogPrimitive.Portal>
   );
 };
+
+type AlertDialogActionProps = CP<
+  AlertDialogPrimitive.AlertDialogActionProps & { cancelProps: AlertDialogPrimitive.AlertDialogCancelProps },
+  string
+>;
