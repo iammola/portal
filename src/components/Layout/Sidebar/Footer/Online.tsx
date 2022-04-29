b25c5f8447d57d2c5ee249e95156c35c0cbc297b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-30 00:23:04 +0100

    fix: use correct color classes 🎨

--- src/components/Layout/Sidebar/Footer/Online.tsx
+++ src/components/Layout/Sidebar/Footer/Online.tsx
@@ -5,17 +5,23 @@ export const Online: React.FC = () => {
 
   return (
     <div className="group absolute top-0 right-0 h-3 w-3 cursor-pointer">
-      <div className={cx("h-3 w-3 rounded-full ring ring-white", [online, "bg-green-600", "bg-red-600"])} />
+      <div
+        className={cx("h-3 w-3 rounded-full ring ring-white", [
+          online,
+          "bg-green-9 dark:bg-green-dark-9",
+          "bg-red-9 dark:bg-red-dark-9",
+        ])}
+      />
       <div
         className={cx("absolute inset-0 h-3 w-3 animate-ping rounded-full opacity-75", [
           online,
-          "bg-green-500",
-          "bg-red-500",
+          "bg-green-10 dark:bg-green-dark-10",
+          "bg-red-10 dark:bg-red-dark-10",
         ])}
       />
       <div className="absolute left-3 -top-8 hidden min-w-max items-center justify-center gap-x-2 rounded-lg bg-white p-2.5 text-xs shadow group-hover:flex">
         <span>{online ? "👍" : "👎"}</span>{" "}
-        <span className="text-gray-600 tracking-wider">{online ? "Online" : "Offline"}</span>
+        <span className="text-gray-11 tracking-wider">{online ? "Online" : "Offline"}</span>
       </div>
     </div>
   );
