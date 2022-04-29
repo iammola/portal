f9e47e02261662716f4faa2044a60f0dccbf3b9e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-29 03:11:24 +0100

    build(deps): update dependencies 📦

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -1,8 +1,3 @@
-declare module "mongoose-lean-virtuals" {
-  import { Schema } from "mongoose";
-  export default function mongooseLeanVirtuals(schema: Schema): void;
-}
-
 declare module "if-emoji" {
   export default function ifEmoji(emoji: string): boolean;
 }
