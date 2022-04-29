6f89e177e4a28b1a556acc7ed423a462345a1d3a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-29 02:09:13 +0100

    enh(types): omit `children` property from `CP` type ⚡️

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,5 @@
 export {};
 
 declare global {
-  export type CP<P = unknown, C extends React.ReactNode = React.ReactNode> = { children?: C } & P;
+  export type CP<P = unknown, C extends React.ReactNode = React.ReactNode> = { children?: C } & Omit<P, "children">;
 }
