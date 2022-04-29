35e9250784008f0a72adce4a1ecaf8a763e6f1b0
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-29 02:29:53 +0100

    feat(alert): add `Title` and `Description` styles ✨

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,13 +4,37 @@ export const AlertDialog = AlertDialogPrimitive.Root;
 
 export const AlertDialogTrigger = AlertDialogPrimitive.Trigger;
 
-export const AlertDialogTitle = AlertDialogPrimitive.Title;
-
-export const AlertDialogDescription = AlertDialogPrimitive.Description;
-
 export const AlertDialogAction = AlertDialogPrimitive.Action;
 
 export const AlertDialogCancel = AlertDialogPrimitive.Cancel;
+
+export const AlertDialogTitle: React.FC<CP<AlertDialogPrimitive.AlertDialogTitleProps, string>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <AlertDialogPrimitive.Title
+      {...props}
+      className="text-lg font-medium text-gray-12"
+    >
+      {children}
+    </AlertDialogPrimitive.Title>
+  );
+};
+
+export const AlertDialogDescription: React.FC<CP<AlertDialogPrimitive.AlertDialogDescriptionProps, string>> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <AlertDialogPrimitive.Description
+      {...props}
+      className="mb-5 text-sm text-gray-11 dark:text-gray-dark-11"
+    >
+      {children}
+    </AlertDialogPrimitive.Description>
+  );
+};
 
 export const AlertDialogContent: React.FC<CP<AlertDialogPrimitive.AlertDialogContentProps>> = ({
   children,
