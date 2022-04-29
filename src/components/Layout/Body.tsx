9d7d5c20c3377abe2afa2a8aa0931e697e6a765c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-29 23:28:12 +0100

    refactor(layout): use `online` hook in `Sidebar` ♻️

--- src/components/Layout/Body.tsx
+++ src/components/Layout/Body.tsx
@@ -1,7 +1,3 @@
-import { useOnline } from "utils";
-
 export const Body: React.FC<CP> = ({ children }) => {
-  useOnline();
-
   return <section className="bg-slate-100 flex h-full grow flex-col justify-start overflow-y-auto">{children}</section>;
 };
--- src/components/Layout/Sidebar/Footer/Online.tsx
+++ src/components/Layout/Sidebar/Footer/Online.tsx
@@ -1,37 +1,21 @@
-import { useState } from "react";
-
-import { cx, useIsomorphicLayoutEffect } from "utils";
+import { cx, useOnline } from "utils";
 
 export const Online: React.FC = () => {
-  const [onLine, setOnLine] = useState(false);
-
-  useIsomorphicLayoutEffect(() => {
-    setOnLine(navigator.onLine);
-    const setOnline = () => setOnLine(true);
-    const setOffline = () => setOnLine(false);
-
-    addEventListener("online", setOnline);
-    addEventListener("offline", setOffline);
-
-    return () => {
-      removeEventListener("online", setOnline);
-      removeEventListener("offline", setOffline);
-    };
-  }, []);
+  const { online } = useOnline();
 
   return (
     <div className="group absolute top-0 right-0 h-3 w-3 cursor-pointer">
-      <div className={cx("h-3 w-3 rounded-full ring ring-white", [onLine, "bg-green-600", "bg-red-600"])} />
+      <div className={cx("h-3 w-3 rounded-full ring ring-white", [online, "bg-green-600", "bg-red-600"])} />
       <div
         className={cx("absolute inset-0 h-3 w-3 animate-ping rounded-full opacity-75", [
-          onLine,
+          online,
           "bg-green-500",
           "bg-red-500",
         ])}
       />
       <div className="absolute left-3 -top-8 hidden min-w-max items-center justify-center gap-x-2 rounded-lg bg-white p-2.5 text-xs shadow group-hover:flex">
-        <span>{onLine ? "👍" : "👎"}</span>{" "}
-        <span className="text-gray-600 tracking-wider">{onLine ? "Online" : "Offline"}</span>
+        <span>{online ? "👍" : "👎"}</span>{" "}
+        <span className="text-gray-600 tracking-wider">{online ? "Online" : "Offline"}</span>
       </div>
     </div>
   );
