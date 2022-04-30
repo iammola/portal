71559318fd371d797105a1973c8f15716bb7f96a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-30 17:38:50 +0100

    🚧wip (sidebar): add NaviagtionMenu primitive

--- src/components/Layout/Sidebar/Menu/index.tsx
+++ src/components/Layout/Sidebar/Menu/index.tsx
@@ -1,3 +1,10 @@
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
+
 export const Menu: React.FC = () => {
-  return <></>;
+  return (
+    <NavigationMenuPrimitive.Root orientation="vertical">
+      <NavigationMenuPrimitive.List className="w-full grow space-y-2 p-4" />
+      <NavigationMenuPrimitive.Viewport />
+    </NavigationMenuPrimitive.Root>
+  );
 };
