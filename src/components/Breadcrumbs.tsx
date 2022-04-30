ddef0a1817134507b9085e1b32522ff69e5134bf
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-30 05:16:18 +0100

    ✨feat (breadcrumbs): add `Home` and `Path` components

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,3 +1,28 @@
+import Link from "next/link";
+import { CaretRightIcon, HomeIcon } from "@radix-ui/react-icons";
+
 export const Breadcrumbs: React.FC = () => {
   return <></>;
+};
+
+const Home: React.FC = () => {
+  return (
+    <Link href="/">
+      <a className="flex gap-2 text-gray-11 items-center justify-start text-sm">
+        <HomeIcon />
+        Home
+      </a>
+    </Link>
+  );
+};
+
+const Path: React.FC<CP<{ href: string }, string>> = ({ children, href }) => {
+  return (
+    <Link href={href}>
+      <a className="flex gap-2 text-gray-11 items-center justify-start text-sm">
+        <CaretRightIcon />
+        {children}
+      </a>
+    </Link>
+  );
 };
