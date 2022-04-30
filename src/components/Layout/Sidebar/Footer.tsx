1605a82370782eb8b0afd55a7818ffed45ad6c66
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-30 06:05:04 +0100

    ♻️refactor (sidebar): move `Online` component into `Footer`

--- src/components/Layout/Sidebar/Footer.tsx
+++ src/components/Layout/Sidebar/Footer.tsx
@@ -1,6 +1,26 @@
+import { Avatar } from "components";
 import { cx, useOnline } from "utils";
 
-export const Online: React.FC = () => {
+export const Footer: React.FC = () => {
+  return (
+    <div className="flex w-full items-center gap-x-4 p-4">
+      <div className="relative">
+        <Avatar
+          initials="RP"
+          src="/Users/005.jpg"
+          name="Rebecca Pearson"
+        />
+        <Online />
+      </div>
+      <div>
+        <div className="text-gray-12 truncate text-sm">Rebecca Pearson</div>
+        <div className="text-gray-11 truncate text-xs tracking-wide">rebecca.pearson@thisisus.com</div>
+      </div>
+    </div>
+  );
+};
+
+const Online: React.FC = () => {
   const { online } = useOnline();
 
   return (
