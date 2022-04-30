d23829ae606c3d8fca4e2c7b8e1c18b360823a4d
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-30 21:42:19 +0100

    💄ui (layout): change body background-color

--- src/components/Layout/Body.tsx
+++ src/components/Layout/Body.tsx
@@ -1,6 +1,6 @@
 export const Body: React.FC<CP> = ({ children }) => {
   return (
-    <section className="bg-white dark:bg-slate-dark-2 flex h-full grow flex-col justify-start overflow-y-auto">
+    <section className="bg-white dark:bg-slate-dark-1 flex h-full grow flex-col justify-start overflow-y-auto">
       {children}
     </section>
   );
