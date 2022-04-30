93261b2f772cfd658fcf61680a30a3a19c8eca74
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-30 17:50:19 +0100

    🚧wip (sidebar): add user navigation data

--- src/components/Layout/Sidebar/Menu.tsx
+++ src/components/Layout/Sidebar/Menu.tsx
@@ -1,6 +1,19 @@
+import { useMemo } from "react";
+import { getCookie } from "cookies-next";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 
+import { USER_COOKIE } from "utils";
+
+import navigation from "./navigation.json";
+
 export const Menu: React.FC = () => {
+  const list = useMemo<Array<MenuLink | MenuList>>(() => {
+    const level = getCookie(USER_COOKIE);
+    if (!level) return [];
+
+    return navigation[level as keyof typeof navigation];
+  }, []);
+
   return (
     <NavigationMenuPrimitive.Root orientation="vertical">
       <NavigationMenuPrimitive.List className="w-full grow space-y-2 p-4" />
@@ -8,3 +21,15 @@ export const Menu: React.FC = () => {
     </NavigationMenuPrimitive.Root>
   );
 };
+
+interface MenuLink {
+  kind: "link";
+  href: string;
+  title: string;
+}
+
+interface MenuList {
+  kind: "list";
+  title: string;
+  list: MenuLink[];
+}
