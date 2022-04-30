76556340c85cb8cfecb859f65e8661bb5278fd27
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-30 05:58:47 +0100

    ✨feat (breadcrumbs): add paths logic

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,8 +1,33 @@
 import Link from "next/link";
+import { useMemo } from "react";
+import { useRouter } from "next/router";
 import { CaretRightIcon, HomeIcon } from "@radix-ui/react-icons";
 
 export const Breadcrumbs: React.FC = () => {
-  return <></>;
+  const { asPath, isReady } = useRouter();
+  const paths = useMemo(() => {
+    const query = asPath.indexOf("?") > 0 ? asPath.indexOf("?") : asPath.length;
+    const keys = asPath.slice(1, query).replaceAll("-", " ").split("/");
+
+    return keys.reduce<string[]>((acc, _, idx) => {
+      return [...acc, keys.slice(0, idx + 1).join("/")];
+    }, []);
+  }, [asPath]);
+
+  if (!isReady) return null;
+  return (
+    <div className="flex gap-2 items-center justify-start">
+      <Home />
+      {paths.map((path) => (
+        <Path
+          key={path}
+          href={path}
+        >
+          {path.replace(/(?:.*?\/)+/, "")}
+        </Path>
+      ))}
+    </div>
+  );
 };
 
 const Home: React.FC = () => {
