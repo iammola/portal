c20219533331487cde8d6512844eb1a114e80a10
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-04 00:03:16 +0100

    💄ui (sidebar): add blur bg and styles

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -9,17 +9,18 @@ export const Sidebar: React.FC = () => {
   const { online } = useOnline();
 
   return (
-    <aside className="divide-slate-300 flex h-full w-[22.5rem] shrink-0 flex-col items-start justify-start divide-y overflow-y-auto">
+    <aside className="fixed inset-0 z-50 grid h-screen w-screen max-w-[20rem] grid-rows-[minmax(0,_1fr)_repeat(2,max-content)] overflow-x-auto overflow-y-auto bg-gray-2 dark:bg-gray-dark-2 md:h-full lg:relative">
       <Menu />
       <Separator.Root className="h-px w-full bg-gray-6" />
       <Footer online={online} />
+      <div className="fixed right-0 top-0 z-[-1] hidden h-screen w-[calc(100vw-20rem)] bg-transparent backdrop-blur sm:block lg:hidden" />
     </aside>
   );
 };
 
 const Footer: React.FC<{ online: boolean }> = ({ online }) => {
   return (
-    <div className="flex w-full items-center gap-x-4 p-4">
+    <div className="flex w-full items-center gap-4 p-4">
       <div className="relative rounded-full ring-1 ring-gray-6 dark:ring-gray-dark-6">
         <Avatar
           initials="RP"
@@ -35,8 +36,10 @@ const Footer: React.FC<{ online: boolean }> = ({ online }) => {
         />
       </div>
       <div>
-        <div className="truncate text-sm text-gray-12">Rebecca Pearson</div>
-        <div className="truncate text-xs tracking-wide text-gray-11">rebecca.pearson@thisisus.com</div>
+        <div className="truncate text-sm font-medium text-gray-12 dark:text-gray-dark-12">Rebecca Pearson</div>
+        <div className="truncate text-xs tracking-wide text-gray-11 dark:text-gray-dark-11">
+          rebecca.pearson@thisisus.com
+        </div>
       </div>
     </div>
   );
