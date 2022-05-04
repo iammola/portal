b78223f072d8b928ea704decfd9b250c68ac1c15
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-04 23:56:44 +0100

    🔥prune: remove `all-old` folder

--- all-old/components/Class/index.ts
+++
@@ -1,5 +0,0 @@
-export { Create } from "./Create";
-export { Students } from "./Students";
-export { Subjects } from "./Subjects";
-export { Teachers } from "./Teachers";
-export { Classes as ClassesList } from "./Classes";
--- all-old/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { List } from "./List";
-export { Layout } from "./Layout";
-export { UserImage } from "./UserImage";
-export { Breadcrumbs } from "./Breadcrumbs";
