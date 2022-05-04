fc29a62c5ec104ad24c95dbec5f60b5595cc7679
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-04 03:24:32 +0100

    💄ui (sidebar): increase breakpoint

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -13,9 +13,9 @@ export const Sidebar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    /** Opens the sidebar if the window width is more than 768px (md), closes it otherwise */
+    /** Opens the sidebar if the window width is more than 1024px (lg/desktop), closes it otherwise */
     function handleResize() {
-      const md = window.matchMedia("(min-width: 768px)").matches;
+      const md = window.matchMedia("(min-width: 1024px)").matches;
       setIsOpen((open) => (md ? true : open));
     }
 
@@ -28,7 +28,7 @@ export const Sidebar: React.FC = () => {
     <Fragment>
       <aside
         className={cx(
-          "fixed inset-0 z-50 grid h-screen w-screen grid-rows-[max-content_minmax(0,_1fr)_repeat(2,max-content)] overflow-x-auto overflow-y-auto bg-gray-2 dark:bg-gray-dark-2 xs:max-w-[20rem] md:relative md:h-full md:grid-rows-[minmax(0,_1fr)_repeat(2,max-content)]",
+          "fixed inset-0 z-50 grid h-screen w-screen shrink-0 grid-rows-[max-content_minmax(0,_1fr)_repeat(2,max-content)] overflow-x-auto overflow-y-auto bg-gray-2 dark:bg-gray-dark-2 xs:max-w-[20rem] lg:relative lg:grid-rows-[minmax(0,_1fr)_repeat(2,max-content)]",
           [!isOpen, "w-0"]
         )}
       >
@@ -36,7 +36,7 @@ export const Sidebar: React.FC = () => {
           <button
             type="button"
             onClick={() => setIsOpen(false)}
-            className="m-3 inline-flex items-center gap-2 justify-self-end rounded-full bg-gray-3 px-4 py-2 text-xs tracking-wide text-gray-12 shadow-md hover:bg-gray-4 active:bg-gray-5  dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:hover:bg-gray-dark-4 dark:active:bg-gray-dark-5 md:hidden"
+            className="m-3 inline-flex items-center gap-2 justify-self-end rounded-full bg-gray-3 px-4 py-2 text-xs tracking-wide text-gray-12 shadow-md hover:bg-gray-4 active:bg-gray-5  dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:hover:bg-gray-dark-4 dark:active:bg-gray-dark-5 lg:hidden"
           >
             Close
             <Cross2Icon />
@@ -45,7 +45,7 @@ export const Sidebar: React.FC = () => {
         <Menu />
         <SeparatorPrimitive.Root className="h-px w-full bg-gray-6" />
         <Footer online={online} />
-        <div className="fixed right-0 top-0 z-[-1] hidden h-screen w-[calc(100vw-20rem)] bg-transparent backdrop-blur xs:block md:hidden" />
+        <div className="fixed right-0 top-0 z-[-1] hidden h-screen w-[calc(100vw-20rem)] bg-transparent backdrop-blur xs:block lg:hidden" />
       </aside>
       {!isOpen && (
         <TooltipPrimitive.Root delayDuration={100}>
