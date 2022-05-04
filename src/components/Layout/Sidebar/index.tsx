b48b133d9de5b85627834ecf8592e0c47bcc3826
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-04 01:55:47 +0100

    💄ui (sidebar): add close button

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { Cross2Icon } from "@radix-ui/react-icons";
 import * as Separator from "@radix-ui/react-separator";
 
 import { Avatar } from "components";
@@ -9,7 +10,11 @@ export const Sidebar: React.FC = () => {
   const { online } = useOnline();
 
   return (
-    <aside className="fixed inset-0 z-50 grid h-screen w-screen grid-rows-[minmax(0,_1fr)_repeat(2,max-content)] overflow-x-auto overflow-y-auto bg-gray-2 dark:bg-gray-dark-2 xs:max-w-[20rem] md:relative md:h-full">
+    <aside className="fixed inset-0 z-50 grid h-screen w-screen grid-rows-[max-content_minmax(0,_1fr)_repeat(2,max-content)] overflow-x-auto overflow-y-auto bg-gray-2 dark:bg-gray-dark-2 xs:max-w-[20rem] md:relative md:h-full md:grid-rows-[minmax(0,_1fr)_repeat(2,max-content)]">
+      <button className="m-3 inline-flex items-center gap-2 justify-self-end rounded-full bg-gray-3 px-4 py-2 text-xs tracking-wide text-gray-12 shadow-md hover:bg-gray-4 active:bg-gray-5  dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:hover:bg-gray-dark-4 dark:active:bg-gray-dark-5 md:hidden">
+        Close
+        <Cross2Icon />
+      </button>
       <Menu />
       <Separator.Root className="h-px w-full bg-gray-6" />
       <Footer online={online} />
