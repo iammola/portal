95024885ad65e72edfa7a96c677eef4927007dc5
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-05 13:33:02 +0100

    ⚰️rip (fonts): remove Poppins font link

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,7 @@ export const Input: React.FC<InputProps> = ({ children, id, onChange, ...props }
     <div className="flex flex-col items-start justify-center gap-1">
       <LabelPrimitive.Root
         htmlFor={id || customId}
-        className="select-none text-sm font-medium tracking-wide text-gray-11 dark:text-gray-dark-11"
+        className="select-none text-sm font-medium tracking-wide text-gray-12 dark:text-gray-dark-12"
       >
         {children}
       </LabelPrimitive.Root>
@@ -17,7 +17,7 @@ export const Input: React.FC<InputProps> = ({ children, id, onChange, ...props }
         type="text"
         id={id || customId}
         onChange={(e) => onChange(e.target.value)}
-        className="inline-flex h-[45px] w-full min-w-[300px] items-center justify-center rounded bg-gray-2 px-2.5 text-sm text-gray-11 focus:outline-none focus:ring-2 focus:ring-gray-8 dark:bg-gray-dark-2 dark:text-gray-dark-11 dark:ring-gray-dark-7 dark:focus:ring-gray-dark-8"
+        className="inline-flex h-[45px] w-full min-w-[300px] items-center justify-center rounded bg-gray-2 px-2.5 text-sm text-gray-12 focus:outline-none focus:ring-2 focus:ring-gray-8 dark:bg-gray-dark-2 dark:text-gray-dark-12 dark:ring-gray-dark-7 dark:focus:ring-gray-dark-8"
       />
     </div>
   );
