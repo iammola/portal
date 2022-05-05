c256d6b4c95da5dee85bded900e8e783840ce186
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-05 21:49:46 +0100

    🐛fix (avatar): use default Avatar Image
    NextJS's Image component won't work here because it doesn't render the `Image` as the first child for all layout props except from `raw` which doesn't give any of the responsive pros

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,13 @@
-import Image from "next/image";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
 export const Avatar: React.FC<AvatarProps> = (props) => {
   return (
     <AvatarPrimitive.Root className="inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full bg-black-a-3 align-middle dark:bg-white-a-3">
-      <AvatarPrimitive.Image asChild>
-        <Image
-          layout="fill"
-          src={props.src}
-          alt={props.name}
-          className="h-full w-full rounded-[inherit] object-cover"
-        />
-      </AvatarPrimitive.Image>
+      <AvatarPrimitive.Image
+        src={props.src}
+        alt={props.name}
+        className="h-full w-full rounded-[inherit] object-cover"
+      />
       <AvatarPrimitive.Fallback
         delayMs={600}
         className="grid h-full w-full place-items-center bg-white text-sm font-medium text-black dark:bg-black dark:text-white"
