4b8c08a7c8e50f9080e6d91f10dec296ea9ce19d
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-06 14:04:17 +0100

    🔥prune (hooks): remove `useCountryFlag hook

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,6 +1,5 @@
 export { useTabs } from "./use-tabs";
 export { useOnline } from "./use-online";
 export { useIsChanging } from "./use-is-changing";
-export { useCountryFlag } from "./use-country-flag";
 export { useTraceUpdate } from "./use-trace-update";
 export { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect";
