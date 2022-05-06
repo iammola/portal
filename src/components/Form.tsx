e98769fe6bafa1d2c3a7591b80bca21e0689696a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-06 13:08:29 +0100

    🚧wip (form): add Phone component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -123,6 +123,29 @@ export const Password: React.FC<InputProps> = ({ children, id, onChange, ...prop
   );
 };
 
+export const Phone: React.FC<InputProps> = ({ children, id, onChange, ...props }) => {
+  const customId = useId();
+
+  return (
+    <div className="flex flex-col items-start justify-center gap-1">
+      <LabelPrimitive.Root
+        htmlFor={id || customId}
+        className="flex w-full select-none items-center justify-between gap-4"
+      >
+        <span className="text-sm font-medium tracking-wide text-gray-12 dark:text-gray-dark-12">{children}</span>
+        {!props.required && <span className="text-xs text-gray-11 dark:text-gray-dark-11">Optional</span>}
+      </LabelPrimitive.Root>
+      <input
+        {...props}
+        type="text"
+        id={id || customId}
+        onChange={(e) => onChange(e.target.value)}
+        className="inline-flex h-[45px] w-full min-w-[300px] items-center justify-center rounded bg-gray-2 px-2.5 text-sm text-gray-12 focus:outline-none focus:ring-2 focus:ring-gray-8 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:ring-gray-dark-7 dark:focus:ring-gray-dark-8"
+      />
+    </div>
+  );
+};
+
 export const Select: Select = ({ children, label, ...props }) => {
   return (
     <LabelPrimitive.Root className="flex flex-col items-start justify-center gap-1">
