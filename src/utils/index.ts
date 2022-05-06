318f7f7168f847f344e578b29c0f629be3eb2541
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-06 14:44:17 +0100

    🔨build (utils): add `getFlagEmoji` function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,18 @@ export * from "./api";
 export * from "./hooks";
 export * from "./password";
 export * from "./constants";
+
+/**
+ * It takes a country code, converts it to uppercase, splits it into an array of characters, maps each
+ * character to its corresponding code point, and then returns a string of the code points
+ * @param {string} countryCode - The country code of the country you want to get the flag emoji for.
+ * @returns An emoji for the country code passed in.
+ */
+export function getFlagEmoji(countryCode: string) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt(0));
+
+  return String.fromCodePoint(...codePoints);
+}
