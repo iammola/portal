a0f259a86b4f01afedee6cee89e09d05576ff33a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-06 02:55:09 +0100

    🔨build (utils): add `checkPasswordStrength` function

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,5 +1,9 @@
 import { pbkdf2Sync, randomBytes } from "crypto";
 
+import zxcvbnEnPackage from "@zxcvbn-ts/language-en";
+import { zxcvbn, zxcvbnOptions } from "@zxcvbn-ts/core";
+import zxcvbnCommonPackage from "@zxcvbn-ts/language-common";
+
 const PASSWORD_LENGTH = 64;
 const PASSWORD_ITERATIONS = 1e5;
 const PASSWORD_ENCODING = "hex";
@@ -31,4 +35,23 @@ export function comparePassword(attempt: string, { salt, hash }: Schemas.User.Pa
   );
 
   return hash === attemptHash;
+}
+
+/**
+ * It sets the options for the zxcvbn library, and then returns a function that calls zxcvbn to check the strength of the password
+ * @returns A function that takes a password and returns a zxcvbn object.
+ */
+export function checkPasswordStrength() {
+  const options = {
+    translations: zxcvbnEnPackage.translations,
+    graphs: zxcvbnCommonPackage.adjacencyGraphs,
+    dictionary: {
+      ...zxcvbnCommonPackage.dictionary,
+      ...zxcvbnEnPackage.dictionary,
+    },
+  };
+
+  zxcvbnOptions.setOptions(options);
+
+  return (password: string) => zxcvbn(password);
 }
