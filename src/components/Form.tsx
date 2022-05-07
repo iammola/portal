5c037d027b114b6fe7bebefad88f2b0b4b9a79bb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-07 13:56:46 +0100

    ✨feat (form): add `RadioGroup` component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,7 @@ import * as LabelPrimitive from "@radix-ui/react-label";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import { useCallback, useEffect, useId, useMemo, useState } from "react";
 import {
   CheckIcon,
@@ -282,6 +283,36 @@ export const Checkbox: React.FC<CheckboxProps> = ({ children, id, ...props }) =>
   );
 };
 
+export const RadioGroup: RadioGroup = ({ children, ...props }) => {
+  return (
+    <RadioGroupPrimitive.Root {...props} className="flex flex-col items-start gap-2">
+      {children}
+    </RadioGroupPrimitive.Root>
+  );
+};
+
+RadioGroup.Item = function Item({ children, id, ...props }) {
+  const customId = useId();
+
+  return (
+    <div className="flex items-center gap-2.5">
+      <RadioGroupPrimitive.Item
+        {...props}
+        id={id || customId}
+        className="grid h-6 w-6 place-items-center rounded-full bg-gray-3 hover:bg-gray-4 focus:outline-none focus:ring-2 focus:ring-gray-5 active:bg-gray-5 dark:bg-gray-dark-3 dark:hover:bg-gray-dark-4 dark:focus:ring-gray-dark-5 dark:active:bg-gray-dark-5"
+      >
+        <RadioGroupPrimitive.Indicator className="h-2.5 w-2.5 rounded-full bg-gray-11 dark:bg-gray-dark-11" />
+      </RadioGroupPrimitive.Item>
+      <LabelPrimitive.Root
+        htmlFor={id || customId}
+        className="select-none text-sm tracking-wide text-gray-12 dark:text-gray-dark-12"
+      >
+        {children}
+      </LabelPrimitive.Root>
+    </div>
+  );
+};
+
 interface InputProps extends Omit<React.ComponentProps<"input">, "onChange" | "value"> {
   value?: string;
   children: string;
@@ -303,4 +334,8 @@ interface SelectProps {
 
 interface Select extends React.FC<CP<Parameters<typeof SelectPrimitive.Root>[0] & SelectProps>> {
   Item: React.FC<Parameters<typeof SelectPrimitive.Item>[0]>;
+}
+
+interface RadioGroup extends React.FC<CP<Parameters<typeof RadioGroupPrimitive.Root>[0]>> {
+  Item: React.FC<CP<Parameters<typeof RadioGroupPrimitive.Item>[0], string>>;
 }
