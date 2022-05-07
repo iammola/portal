76eea2594e6d180d9228233f62523f2e6ae672c0
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-07 18:04:16 +0100

    🚧wip (form-users): add preview toggle

--- src/components/Form/Users.tsx
+++ src/components/Form/Users.tsx
@@ -1,17 +1,32 @@
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { useId } from "react";
+import { Fragment, useId, useState } from "react";
+import { EyeOpenIcon, Pencil1Icon } from "@radix-ui/react-icons";
 
 export const Users: React.FC<UsersProps> = ({ children, id, onChange, ...props }) => {
   const customId = useId();
+  const [preview, setPreview] = useState(false);
 
   return (
-    <div className="flex flex-col items-start justify-center gap-1">
-      <LabelPrimitive.Root
-        htmlFor={id || customId}
-        className="flex w-full select-none items-center justify-between gap-4"
-      >
-        <span className="text-sm font-medium tracking-wide text-gray-12 dark:text-gray-dark-12">{children}</span>
+    <div className="flex flex-col items-start justify-center gap-2">
+      <LabelPrimitive.Root htmlFor={id || customId} className="flex w-full select-none items-center gap-1.5">
+        <span className="grow text-sm font-medium tracking-wide text-gray-12 dark:text-gray-dark-12">{children}</span>
         {!props.required && <span className="text-xs text-gray-11 dark:text-gray-dark-11">Optional</span>}
+        <button
+          onClick={() => setPreview(!preview)}
+          className="inline-flex items-center gap-2 rounded px-2 py-1.5 text-xs tracking-wide text-gray-12 hover:bg-gray-4 active:bg-gray-5 dark:text-gray-dark-12 dark:hover:bg-gray-dark-4 dark:active:bg-gray-dark-5"
+        >
+          {preview ? (
+            <Fragment>
+              <Pencil1Icon />
+              Edit
+            </Fragment>
+          ) : (
+            <Fragment>
+              <EyeOpenIcon />
+              Preview
+            </Fragment>
+          )}
+        </button>
       </LabelPrimitive.Root>
       <textarea
         {...props}
