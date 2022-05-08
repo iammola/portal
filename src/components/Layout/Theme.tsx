1a1353d2f135f652a1f8edc80954fe44f82b959d
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-08 10:42:04 +0100

    ♻️refactor (theme-picker): use native button

--- src/components/Layout/Theme.tsx
+++ src/components/Layout/Theme.tsx
@@ -25,16 +25,13 @@ export const ThemePicker: React.FC = () => {
 
   return (
     <Tooltip.Root delayDuration={100}>
-      <Tooltip.Trigger asChild>
-        <button
-          type="button"
-          onClick={toggleTheme}
-          className="fixed bottom-4 right-4 z-[9999] grid place-items-center rounded-full bg-gray-3 p-3 text-gray-12 ring-1 ring-gray-7 hover:bg-gray-4 hover:ring-gray-8 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:ring-gray-dark-7 dark:hover:bg-gray-dark-4 dark:hover:ring-gray-dark-8"
-        >
-          {theme === "dark" && <MoonIcon className="h-6 w-6" />}
-          {theme === "light" && <SunIcon className="h-6 w-6" />}
-          {theme === "system" && <DesktopIcon className="h-6 w-6" />}
-        </button>
+      <Tooltip.Trigger
+        onClick={toggleTheme}
+        className="fixed bottom-4 right-4 z-[9999] grid place-items-center rounded-full bg-gray-3 p-3 text-gray-12 ring-1 ring-gray-7 hover:bg-gray-4 hover:ring-gray-8 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:ring-gray-dark-7 dark:hover:bg-gray-dark-4 dark:hover:ring-gray-dark-8"
+      >
+        {theme === "dark" && <MoonIcon className="h-6 w-6" />}
+        {theme === "light" && <SunIcon className="h-6 w-6" />}
+        {theme === "system" && <DesktopIcon className="h-6 w-6" />}
       </Tooltip.Trigger>
       <Tooltip.Content
         side="top"
