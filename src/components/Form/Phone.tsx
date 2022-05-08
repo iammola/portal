b46fa8f38a7efd389140e88c54ba5bec4c7f4ac6
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-08 22:24:33 +0100

    🚸ux (form-phone): add `autocomplete` prop

--- src/components/Form/Phone.tsx
+++ src/components/Form/Phone.tsx
@@ -56,6 +56,7 @@ export const Phone: React.FC<PhoneProps> = ({ children, id, onChange, ...props }
             {...props}
             type="tel"
             value={formatted}
+            autoComplete="tel"
             id={id || customId}
             onChange={(e) => updateValue(e.target.value)}
             className="inline-flex h-[45px] w-full min-w-[300px] items-center justify-center rounded-r bg-gray-3 px-2.5 text-sm text-gray-12 focus:outline-none focus:ring-2 focus:ring-gray-8 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:ring-gray-dark-7 dark:focus:ring-gray-dark-8"
