d5e03185d6324da1ff66a9f2c47a5b2fe7c691cb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-11 09:24:40 +0100

    🔥prune (hooks): remove unused `isChanging` and `traceUpdate` hooks

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,5 +1,3 @@
 export { useTabs } from "./use-tabs";
 export { useOnline } from "./use-online";
-export { useIsChanging } from "./use-is-changing";
-export { useTraceUpdate } from "./use-trace-update";
 export { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect";
