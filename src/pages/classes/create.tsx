79fdab410be66970b2da0e897d2d17420cadc98f
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-13 16:12:07 +0100

    🚧wip: add heading and form

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -9,6 +9,18 @@ const CreateClass: NextPage = () => {
       <Head>
         <title>Create a Class &middot; Portal</title>
       </Head>
+      <div className="flex w-full grow flex-col items-center justify-center gap-8">
+        <h3 className="text-2xl font-bold text-gray-12 dark:text-gray-dark-12">Create a Class</h3>
+        <form className="w-full max-w-xs space-y-10">
+          <div className="space-y-7"></div>
+          <button
+            type="submit"
+            className="w-full rounded-lg bg-black-a-9 p-3 text-white shadow-lg hover:bg-black-a-10 dark:text-gray-dark-12"
+          >
+            Create Class
+          </button>
+        </form>
+      </div>
     </Fragment>
   );
 };
