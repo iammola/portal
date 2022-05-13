d18d8817ace3fb44e8a45bf4ff4dda07f8673c7b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-13 23:33:42 +0100

    🧑‍💻dx: add `fetchAPIEndpoint` overloads to force init options

--- src/api/endpoint.ts
+++ src/api/endpoint.ts
@@ -5,20 +5,26 @@ import { JWT_COOKIE_TOKEN } from "utils";
 /**
  * It takes an endpoint, and an optional body, and returns the response data from the API
  * @param {RequestInfo} endpoint - The URL to fetch.
- * @param obj - An object with two properties, body and init
+ * @param init - An object to initialize the fetch request
  * @returns the API response data
  */
-export async function fetchAPIEndpoint<ResponseData, Body = undefined>(
+export async function fetchAPIEndpoint<ResponseData>(
   endpoint: RequestInfo,
-  { body, ...init }: Omit<RequestInit, "body"> & { body?: Body } = {}
-) {
+  init?: Omit<RequestInit, "body"> & { body: never; method: "GET" | "DELETE" }
+): Promise<API.Result<ResponseData>>;
+export async function fetchAPIEndpoint<ResponseData, Body>(
+  endpoint: RequestInfo,
+  init: Omit<RequestInit, "body"> & { body: Body; method: "POST" | "PUT" | "SEARCH" }
+): Promise<API.Result<ResponseData>>;
+
+export async function fetchAPIEndpoint<ResponseData>(endpoint: RequestInfo, { body, ...init }: RequestInit = {}) {
   const JWT = getCookie(JWT_COOKIE_TOKEN);
 
   const response = await fetch(endpoint, {
     ...init,
     body: JSON.stringify(body),
     headers: {
-      ...init?.headers,
+      ...init.headers,
       Accept: "application/json",
       Authorization: JWT ? `Bearer ${JWT.toString()}` : "",
     },
