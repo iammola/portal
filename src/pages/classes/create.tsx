3db41e3851c94dd6fc82ee8cc2f0076ac67c6c94
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-13 16:29:35 +0100

    🚧wip: add form elements

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -1,9 +1,14 @@
 import Head from "next/head";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
+import { Input, Users } from "components/Form";
 
 import type { NextPage } from "next";
 
 const CreateClass: NextPage = () => {
+  const [teachers, setTeachers] = useState("");
+  const [name, setName] = useState({ long: "", short: "", special: "" });
+
   return (
     <Fragment>
       <Head>
@@ -12,7 +17,20 @@ const CreateClass: NextPage = () => {
       <div className="flex w-full grow flex-col items-center justify-center gap-8">
         <h3 className="text-2xl font-bold text-gray-12 dark:text-gray-dark-12">Create a Class</h3>
         <form className="w-full max-w-xs space-y-10">
-          <div className="space-y-7"></div>
+          <div className="space-y-7">
+            <Input required value={name.long} onChange={(long) => setName((name) => ({ ...name, long }))}>
+              Name
+            </Input>
+            <Input required value={name.short} onChange={(short) => setName((name) => ({ ...name, short }))}>
+              Alias
+            </Input>
+            <Input required value={name.special} onChange={(special) => setName((name) => ({ ...name, special }))}>
+              Special name
+            </Input>
+            <Users value={teachers} onChange={setTeachers}>
+              Teachers
+            </Users>
+          </div>
           <button
             type="submit"
             className="w-full rounded-lg bg-black-a-9 p-3 text-white shadow-lg hover:bg-black-a-10 dark:text-gray-dark-12"
