22c807ce2bb0d04c2d274df6b1db10ec210e8e6d
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-13 16:02:26 +0100

    🚧wip: add create classes page

---
+++ src/pages/classes/create.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+import type { NextPage } from "next";
+
+const CreateClass: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Create a Class &middot; Portal</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default CreateClass;
