a88e9e0068cd9a3b698f277ce60a64fdd73a3bc6
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-13 16:35:59 +0100

    💄ui: add padding to content

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -14,7 +14,7 @@ const CreateClass: NextPage = () => {
       <Head>
         <title>Create a Class &middot; Portal</title>
       </Head>
-      <div className="flex w-full grow flex-col items-center justify-center gap-8">
+      <div className="flex w-full grow flex-col items-center justify-center gap-8 py-10 px-6">
         <h3 className="text-2xl font-bold text-gray-12 dark:text-gray-dark-12">Create a Class</h3>
         <form className="w-full max-w-xs space-y-10">
           <div className="space-y-7">
