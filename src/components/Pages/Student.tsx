8281d599547c3d7e01345c91c673d4d45701f0eb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-16 15:36:59 +0100

    🐛fix: set initial term and class state

--- src/components/Pages/Student.tsx
+++ src/components/Pages/Student.tsx
@@ -1,28 +1,35 @@
 import { Fragment } from "react";
 
+import { useIsomorphicLayoutEffect } from "hooks";
 import { Checkbox, Select } from "components/Form";
 
 export const AcademicRecord: React.FC<AcademicRecordProps> = ({ updateSubjects, updateTerm, ...props }) => {
+  // Fetch data from API
   const terms: API.Term.GET.AllData = [];
   const classes: API.Class.GET.AllData["classes"] = [];
   const subjects: API.Class.GET.Subjects["subjects"] = [];
 
-  function handleClassChange(val: string) {
+  function classChange(val: string) {
     props.updateClass(val);
     updateSubjects([]);
   }
 
+  useIsomorphicLayoutEffect(() => {
+    if (!props.term && terms.length) updateTerm(String(terms[0]._id));
+    if (!props.class && classes.length) props.updateClass(String(classes[0]._id));
+  }, [classes, props, terms, updateTerm]);
+
   return (
     <Fragment>
       <div className="grid w-full gap-[inherit] md:grid-cols-[repeat(2,max-content)]">
-        <Select required label="Term" value={props.term || String(terms[0]._id)} onValueChange={updateTerm}>
+        <Select required label="Term" value={props.term} onValueChange={updateTerm}>
           {terms.map((item) => (
             <Select.Item key={String(item._id)} value={String(item._id)}>
               {item.name.long}
             </Select.Item>
           ))}
         </Select>
-        <Select required label="Class" value={props.class || String(classes[0]._id)} onValueChange={handleClassChange}>
+        <Select required label="Class" value={props.class} onValueChange={classChange}>
           {classes.map((item) => (
             <Select.Item key={String(item._id)} value={String(item._id)}>
               {item.name.long}
