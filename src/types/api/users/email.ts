92edd4621fc120ea8ea3b93e988666e0e4cd5cc2
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-16 20:52:10 +0100

    🔥prune (types): remove unused type

--- src/types/api/users/email.ts
+++
@@ -1,9 +0,0 @@
-export type UsersEmailRequestBody = {
-  select?: string;
-  mail: string;
-  userType: Schemas.User.Type;
-};
-
-export type UsersEmailData = {
-  name: Pick<Schemas.User.Base["name"], "initials" | "username" | "full">;
-} & Pick<Schemas.User.Base, "_id" | "schoolMail">;
