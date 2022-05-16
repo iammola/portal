cefacd58fe332b635538a33619f240c382065ac0
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-17 00:02:29 +0100

    🔨build: add bundleAnalyzer to nextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+/** @type {import('types').NextBundleAnalyzer} */
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   // swcMinify: true,
   reactStrictMode: true,
   images: { domains: ["drive.google.com"] },
   experimental: { newNextLinkBehavior: true },
+  ...withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })(),
 };
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,12 @@
 export {};
+import type { NextConfig } from "next";
 
 declare global {
   export type CP<P = unknown, C extends React.ReactNode = React.ReactNode> = { children?: C } & (P extends null
     ? unknown
     : Omit<P, "children">);
 }
+
+export type NextBundleAnalyzer = {
+  (opt?: { enabled?: boolean }): (nextConfig: NextConfig) => NextConfig;
+};
