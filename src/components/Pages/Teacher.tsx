44ab68d8de748d8260167a83943469c828b5dfbf
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-18 16:07:49 +0100

    🚧wip: create `ClassTeacher` component

---
+++ src/components/Pages/Teacher.tsx
@@ -0,0 +1,31 @@
+import { Checkbox } from "components/Form";
+
+export const ClassTeacher: React.FC<ClassTeacherProps> = (props) => {
+  // Import data from API
+  const classes: API.Class.GET.AllData["classes"] = [];
+
+  return (
+    <div className="flex flex-wrap items-center justify-start gap-5">
+      {classes.map((item) => (
+        <Checkbox
+          key={String(item._id)}
+          checked={props.selected.includes(String(item._id))}
+          onCheckedChange={(checked) =>
+            props.updateSelected(
+              checked
+                ? [...new Set([...props.selected, String(item._id)])]
+                : props.selected.filter((subject) => subject !== String(item._id))
+            )
+          }
+        >
+          {item.name.long}
+        </Checkbox>
+      ))}
+    </div>
+  );
+};
+
+type ClassTeacherProps = {
+  selected: string[];
+  updateSelected(value: string[]): void;
+};
