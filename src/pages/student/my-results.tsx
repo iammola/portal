d7304c8b9eaf8ce070221d2f1604f21c98653583
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-21 16:53:05 +0100

    🚧wip (student): create `my-results` page

---
+++ src/pages/student/my-results.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
+import type { NextPage } from "next";
+
+const MyResults: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>My Results &middot; Portal</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default MyResults;
