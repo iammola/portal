ae6dc18cdc2383ed9a8efed621942de1fbc20c05
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-21 17:03:48 +0100

    🚧wip: add page heading

--- src/pages/student/my-results.tsx
+++ src/pages/student/my-results.tsx
@@ -9,6 +9,7 @@ const MyResults: NextPage = () => {
       <Head>
         <title>My Results &middot; Portal</title>
       </Head>
+      <h2 className="py-3 text-3xl font-bold tracking-wide text-gray-12 dark:text-gray-dark-12">My Results</h2>
     </Fragment>
   );
 };
