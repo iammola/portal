6437ab52a37bc70948a04522f54894048c8981d7
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-23 04:37:50 +0100

    🎉init: create students page

---
+++ src/pages/teacher/students/index.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Head from "next/head";
+
+import type { NextPage } from "next";
+
+const Students: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Students &middot; Portal</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default Students;
