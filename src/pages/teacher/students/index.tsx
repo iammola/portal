0e67a0ff10ab87bf9c2123cda54362554e7f75cb
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-23 21:07:50 +0100

    ✨feat: add filter toogle

--- src/pages/teacher/students/index.tsx
+++ src/pages/teacher/students/index.tsx
@@ -1,18 +1,19 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { getCookies } from "cookies-next";
 import useSWR from "swr";
 import Head from "next/head";
 
 import { Avatar } from "components";
-import { USER_ID_COOKIE } from "utils";
+import { cx, USER_ID_COOKIE } from "utils";
 
 import type { NextPage } from "next";
 
 const Students: NextPage = () => {
   const cookies = getCookies();
+  const [filter, setFilter] = useState<"" | "all">("");
 
   const { data: students } = useSWR<API.Result<API.Teacher.GET.Students>>(
-    `/api/teachers/${cookies[USER_ID_COOKIE]}/students`
+    `/api/teachers/${cookies[USER_ID_COOKIE]}/students?filter=${filter}`
   );
 
   return (
@@ -20,6 +21,30 @@ const Students: NextPage = () => {
       <Head>
         <title>Students &middot; Portal</title>
       </Head>
+      <div className="flex items-center justify-start gap-2">
+        <button
+          type="button"
+          onClick={() => setFilter("")}
+          className={cx("rounded p-3 ring-1 ring-gray-6 dark:ring-gray-dark-6", [
+            filter === "",
+            "bg-gray-9 text-white hover:bg-gray-10 dark:bg-gray-dark-9 dark:hover:bg-gray-dark-10",
+            "bg-gray-3 text-gray-12 hover:bg-gray-4 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:hover:bg-gray-dark-4",
+          ])}
+        >
+          My Students
+        </button>
+        <button
+          type="button"
+          onClick={() => setFilter("all")}
+          className={cx("rounded p-3 ring-1 ring-gray-6 dark:ring-gray-dark-6", [
+            filter === "all",
+            "bg-gray-9 text-white hover:bg-gray-10 dark:bg-gray-dark-9 dark:hover:bg-gray-dark-10",
+            "bg-gray-3 text-gray-12 hover:bg-gray-4 dark:bg-gray-dark-3 dark:text-gray-dark-12 dark:hover:bg-gray-dark-4",
+          ])}
+        >
+          All Students
+        </button>
+      </div>
       <div className="space-y-3">
         {students?.success &&
           students.data.map((student) => (
