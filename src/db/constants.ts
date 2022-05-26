ff705c395f65fced1b7234f7802f84b47a82b704
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-26 15:09:11 +0100

    ♻️refactor (db): format `ModelNames`

--- src/db/constants.ts
+++ src/db/constants.ts
@@ -1,14 +1,23 @@
 export enum ModelNames {
   AUTH = "Auth",
+
   TERM = "Term",
+
   CLASS = "Class",
+
   PARENT = "Parent",
+
   RESULT = "RESULT",
-  B_SUBJECT = "base",
+
   STUDENT = "Student",
+
   TEACHER = "Teacher",
+
   SUBJECT = "Subject",
+  B_SUBJECT = "base",
   G_SUBJECT = "group",
+
   SESSION = "Session",
+
   ATTENDANCE = "Attendance",
 }
