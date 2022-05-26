c2f1a80278f1fcdcf8215cf4379490261457efed
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-26 04:37:01 +0100

    🐛fix: correctly handle division subjects

--- src/components/Pages/Student.tsx
+++ src/components/Pages/Student.tsx
@@ -2,12 +2,12 @@ import { Fragment, useState } from "react";
 import useSWR from "swr";
 
 import { useIsomorphicLayoutEffect } from "hooks";
-import { Checkbox, Select } from "components/Form";
+import { Checkbox, RadioGroup, Select } from "components/Form";
 
 export const AcademicRecord: React.FC<AcademicRecordProps> = ({ disabled, updateTerm, ...props }) => {
   const [terms, setTerms] = useState<Option[]>([]);
   const [classes, setClasses] = useState<Option[]>([]);
-  const [subjects, setSubjects] = useState<SubjectOption[]>([]);
+  const [subjects, setSubjects] = useState<SubjectOptions[]>([]);
 
   useSWR<API.Result<API.Term.GET.AllData>>("/api/terms", {
     onSuccess(result) {
@@ -48,19 +48,24 @@ export const AcademicRecord: React.FC<AcademicRecordProps> = ({ disabled, update
     onSuccess(result) {
       if (!result.success) return setSubjects([]);
 
-      const subjectDetails = (obj: Pick<Schemas.Subject.Record, "_id" | "mandatory" | "name">) => ({
-        _id: obj._id,
-        name: obj.name.long,
-        mandatory: obj.mandatory,
-      });
+      const activeSubjects: string[] = [];
+      const subjects = result.data.reduce<SubjectOptions[]>((acc, subject) => {
+        const result =
+          subject.__type === "base"
+            ? ({ _id: subject._id, __type: "base" } as const)
+            : ({
+                __type: "group",
+                divisions: subject.divisions.map((_) => ({ _id: _._id, name: _.name.long })),
+              } as const);
 
-      const subjects = result.data.reduce<SubjectOption[]>((acc, subject) => {
-        const result = subject.__type === "base" ? [subjectDetails(subject)] : subject.divisions.map(subjectDetails);
-        return [...acc, ...result];
+        if (subject.mandatory)
+          activeSubjects.push(String(subject.__type === "base" ? subject._id : subject.divisions[0]._id));
+
+        return [...acc, { ...result, name: subject.name.long, mandatory: subject.mandatory }];
       }, []);
 
       setSubjects(subjects);
-      props.updateSubjects(subjects.filter((d) => d.mandatory).map((subject) => String(subject._id)));
+      props.updateSubjects(activeSubjects);
     },
   });
 
@@ -69,9 +74,16 @@ export const AcademicRecord: React.FC<AcademicRecordProps> = ({ disabled, update
     props.updateSubjects([]);
   }
 
-  function checkedChange(checked: boolean, id: string) {
+  function baseCheckedChange(id: string, checked: boolean) {
     if (checked) return props.updateSubjects([...props.subjects, id]);
-    props.updateSubjects(props.subjects.filter((sub) => sub !== id));
+    return props.updateSubjects(props.subjects.filter((sub) => sub !== id));
+  }
+
+  function groupCheckedChange(id: string, checked?: string) {
+    const filtered = props.subjects.filter((sub) => sub !== checked);
+    if (id !== "none") filtered.push(id);
+
+    props.updateSubjects(filtered);
   }
 
   useIsomorphicLayoutEffect(() => {
@@ -97,16 +109,40 @@ export const AcademicRecord: React.FC<AcademicRecordProps> = ({ disabled, update
         </Select>
       </div>
       <div className="flex flex-wrap items-center justify-start gap-5">
-        {subjects.map((item, idx) => (
-          <Checkbox
-            key={idx}
-            disabled={item.mandatory || disabled}
-            checked={item.mandatory || props.subjects.includes(String(item._id))}
-            onCheckedChange={(c) => checkedChange(c as boolean, String(item._id))}
-          >
-            {item.name}
-          </Checkbox>
-        ))}
+        {subjects.map((item) => {
+          const checked = props.subjects.find((sub) => {
+            if (item.__type === "base") return String(item._id) === sub;
+            return item.divisions.map((_) => String(_._id)).includes(sub);
+          });
+
+          return item.__type === "base" ? (
+            <Checkbox
+              key={item.name}
+              required={item.mandatory}
+              checked={item.mandatory || !!checked}
+              disabled={item.mandatory || disabled}
+              onCheckedChange={(c) => baseCheckedChange(String(item._id), c as boolean)}
+            >
+              {item.name}
+            </Checkbox>
+          ) : (
+            <div key={item.name} className="w-full space-y-1.5">
+              <span className="text-xs font-medium text-gray-11 dark:text-gray-dark-11">{item.name}</span>
+              <RadioGroup
+                value={checked}
+                required={item.mandatory}
+                onValueChange={(value) => groupCheckedChange(value, checked)}
+              >
+                {item.divisions.map((div) => (
+                  <RadioGroup.Item key={div.name} value={String(div._id)}>
+                    {div.name}
+                  </RadioGroup.Item>
+                ))}
+                {!item.mandatory && <RadioGroup.Item value="none">None</RadioGroup.Item>}
+              </RadioGroup>
+            </div>
+          );
+        })}
       </div>
     </Fragment>
   );
@@ -117,9 +153,16 @@ type Option = {
   name: string;
 };
 
-type SubjectOption = {
+type SubjectOption<T> = Omit<Option, "_id"> & {
+  __type: T;
   mandatory?: boolean;
-} & Option;
+};
+
+type SubjectOptions =
+  | (SubjectOption<"base"> & Option)
+  | ({
+      divisions: Option[];
+    } & SubjectOption<"group">);
 
 type AcademicRecordProps = {
   term: string;
