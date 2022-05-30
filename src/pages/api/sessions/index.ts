09bd3e38d6c000a2cb2febdf6e33cdd864f8bda2
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-30 15:17:09 +0100

    🔥prune: remove `sessions` create form and POST API
    Deprecated because of the inline form in term

--- src/pages/api/sessions/index.ts
+++ src/pages/api/sessions/index.ts
@@ -6,10 +6,9 @@ import { SessionModel } from "db/models";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const handler: API.Handler<API.Session.POST.Data | API.Session.GET.AllData> = async (req) => {
+const handler: API.Handler<API.Session.GET.AllData> = async (req) => {
   await connect();
   if (req.method === "GET") return GET();
-  if (req.method === "POST") return POST(req.body);
 
   return null;
 };
@@ -25,20 +24,4 @@ async function GET(): API.HandlerResponse<API.Session.GET.AllData> {
   return [{ data: { current, data }, message: ReasonPhrases.OK }, StatusCodes.OK];
 }
 
-async function POST(body: unknown): API.HandlerResponse<API.Session.POST.Data> {
-  const { name } = JSON.parse(body as string) as API.Session.POST.Body;
-
-  const checks = await Promise.all([
-    SessionModel.exists({ "name.long": name.long }),
-    SessionModel.exists({ "name.short": name.short }),
-  ]);
-
-  if (checks[0] != null) throw new Error(`A session with name ${name.long} already exists`);
-  if (checks[1] != null) throw new Error(`A session with alias ${name.short} already exists`);
-
-  const { _id } = await SessionModel.create({ name });
-
-  return [{ data: { _id }, message: ReasonPhrases.CREATED }, StatusCodes.CREATED];
-}
-
-export default (req: NextApiRequest, res: NextApiResponse) => routeWrapper(req, res, handler, ["POST", "GET"]);
+export default (req: NextApiRequest, res: NextApiResponse) => routeWrapper(req, res, handler, ["GET"]);
