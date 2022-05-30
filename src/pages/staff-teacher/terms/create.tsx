c3476b684e7023f87bd418c5aa2724132277b88c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-05-30 03:55:04 +0100

    🚧wip: add inline session form

--- src/pages/staff-teacher/terms/create.tsx
+++ src/pages/staff-teacher/terms/create.tsx
@@ -1,6 +1,8 @@
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
 import { Fragment, useState } from "react";
 import useSWR from "swr";
 import Head from "next/head";
+import dynamic from "next/dynamic";
 
 import { useToast } from "components";
 import { fetchAPIEndpoint } from "api";
@@ -9,7 +11,11 @@ import { Checkbox, Date as FormDate, Input, Select } from "components/Form";
 
 import type { NextPage } from "next";
 
+const CreateSession = dynamic(() => import("components/Pages/Term").then((_) => _.CreateSession));
+
 const CreateTerm: NextPage = () => {
+  const [newSession, setNewSession] = useState<Record<"long" | "short", string>>();
+
   const toasts = useToast();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -76,40 +82,57 @@ const CreateTerm: NextPage = () => {
       <div className="flex w-full grow flex-col items-center justify-center gap-8 py-10 px-6">
         <h3 className="text-2xl font-bold text-gray-12 dark:text-gray-dark-12">Create a Term</h3>
         <form onSubmit={(e) => void handleSubmit(e)} className="w-full max-w-xs space-y-10">
-          <div className="space-y-7">
-            <div className="w-full space-y-1">
-              <Select required label="Session" value={session} onValueChange={setSession}>
-                {sessions.map((session) => (
-                  <Select.Item key={session._id} value={session._id}>
-                    {session.name}
-                  </Select.Item>
-                ))}
-              </Select>
-              <button
-                type="button"
-                className="text-xs text-gray-11 hover:underline hover:underline-offset-1 focus:outline-none dark:text-gray-dark-11"
-              >
-                Create a new session
-              </button>
-            </div>
-            <div className="flex w-full items-start justify-start gap-3">
-              <div className="w-2/3">
-                <Input required value={name.long} onValueChange={(long) => setName((name) => ({ ...name, long }))}>
-                  Name
-                </Input>
+          <div className="space-y-5">
+            {newSession ? (
+              <div className="space-y-1">
+                <CreateSession name={newSession} onValueChange={setNewSession} />
+                <button
+                  type="button"
+                  onClick={() => setNewSession(undefined)}
+                  className="text-xs text-gray-11 hover:underline hover:underline-offset-1 focus:outline-none dark:text-gray-dark-11"
+                >
+                  Select session
+                </button>
               </div>
-              <div className="w-1/3">
-                <Input required value={name.short} onValueChange={(short) => setName((name) => ({ ...name, short }))}>
-                  Alias
-                </Input>
+            ) : (
+              <div className="w-full space-y-1">
+                <Select required label="Session" value={session} onValueChange={setSession}>
+                  {sessions.map((session) => (
+                    <Select.Item key={session._id} value={session._id}>
+                      {session.name}
+                    </Select.Item>
+                  ))}
+                </Select>
+                <button
+                  type="button"
+                  onClick={() => setNewSession({ long: "", short: "" })}
+                  className="text-xs text-gray-11 hover:underline hover:underline-offset-1 focus:outline-none dark:text-gray-dark-11"
+                >
+                  Create a new session
+                </button>
               </div>
+            )}
+            <SeparatorPrimitive.Root className="h-px w-full bg-gray-6 dark:bg-gray-dark-6" />
+            <div className="space-y-7">
+              <div className="flex w-full items-start justify-start gap-3">
+                <div className="w-2/3">
+                  <Input required value={name.long} onValueChange={(long) => setName((name) => ({ ...name, long }))}>
+                    Name
+                  </Input>
+                </div>
+                <div className="w-1/3">
+                  <Input required value={name.short} onValueChange={(short) => setName((name) => ({ ...name, short }))}>
+                    Alias
+                  </Input>
+                </div>
+              </div>
+              <FormDate required value={start} onValueChange={setStart}>
+                Start Date
+              </FormDate>
+              <Checkbox checked={current} onCheckedChange={(c) => setCurrent(c as boolean)}>
+                Mark as current term?
+              </Checkbox>
             </div>
-            <FormDate required value={start} onValueChange={setStart}>
-              Start Date
-            </FormDate>
-            <Checkbox checked={current} onCheckedChange={(c) => setCurrent(c as boolean)}>
-              Mark as current term?
-            </Checkbox>
           </div>
           <button
             type="submit"
