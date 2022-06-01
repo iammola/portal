4d676cbcf8b2af1fe05036d9148e677f4eaff57c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-01 21:45:18 +0100

    🔨build: use experimental `browsersListForSwc` config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const config = {
-  swcMinify: true,
   reactStrictMode: true,
-  images: { domains: ["drive.google.com"] },
-  experimental: { newNextLinkBehavior: true },
+  images: {
+    domains: ["drive.google.com"],
+  },
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+    newNextLinkBehavior: true,
+  },
 };
 
 module.exports = config;
