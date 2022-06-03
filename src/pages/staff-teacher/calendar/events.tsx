b8243ee43c430984d4d40836c467c70aaba71651
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-03 01:24:22 +0100

    🚧wip (calendar): create events page

---
+++ src/pages/staff-teacher/calendar/events.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+import type { NextPage } from "next";
+
+const Events: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Events &middot; Portal</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default Events;
