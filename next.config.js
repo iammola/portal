c04c84f3f16a9c5aa4936c89a8e95c66f8ba844a
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-03 23:19:56 +0100

    🔨build: turn on webpack experimental `topLevelAwait`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('types').NextBundleAnalyzer} */
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const withBundleAnalyzer = require("@next/bundle-analyzer");
+// const withBundleAnalyzer = require("@next/bundle-analyzer");
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -8,12 +8,18 @@ const config = {
   images: {
     domains: ["drive.google.com"],
   },
+  webpack: (config) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    config.experiments.topLevelAwait = true;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config;
+  },
   experimental: {
     legacyBrowsers: false,
     browsersListForSwc: true,
     newNextLinkBehavior: true,
   },
-  ...withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })(),
+  // ...withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })(),
 };
 
 module.exports = config;
