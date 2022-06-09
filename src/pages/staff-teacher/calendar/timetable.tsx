3d54b8c56ff89bc2775b0647a37fb5fed3679f8f
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-09 14:14:24 +0100

    🚧wip (timetable): create timetable page

---
+++ src/pages/staff-teacher/calendar/timetable.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Fragment } from "react";
+
+import type { NextPage } from "next";
+
+const Timetable: NextPage = () => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Timetable &middot; Portal</title>
+      </Head>
+    </Fragment>
+  );
+};
+
+export default Timetable;
