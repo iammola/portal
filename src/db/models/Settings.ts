40648996de90f80b478781148e914488fd2ec4ec
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-11 22:23:55 +0100

    🗃️db (settings): revert use of capped collection (#222)

--- src/db/models/Settings.ts
+++ src/db/models/Settings.ts
@@ -8,8 +8,21 @@ const SettingsSchema = new Schema<Schemas.Settings.Record, Schemas.Settings.Mode
       type: Boolean,
     },
   },
-  { capped: { max: 1, size: 1024 }, versionKey: false }
+  { versionKey: false }
 );
+
+SettingsSchema.pre("save", async function () {
+  const count = await this.collection.estimatedDocumentCount();
+  if (count > 0) throw new Error("Settings document already exists");
+});
 
 export const SettingsModel = (models[ModelNames.SETTINGS] ??
   model(ModelNames.SETTINGS, SettingsSchema)) as Schemas.Settings.Model;
+
+const SETTINGS_DEFAULT = new SettingsModel({
+  locked: false,
+});
+
+SETTINGS_DEFAULT.save({ validateBeforeSave: true }, function (err) {
+  if (!err) console.warn("Default Settings Saved to DB");
+});
