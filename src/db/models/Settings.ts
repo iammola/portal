f840b17deef28d21523c465f7530ff1649b2ef40
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-12 14:56:42 +0100

    ✨feat (settings): add calendar default time and values to settings

--- src/db/models/Settings.ts
+++ src/db/models/Settings.ts
@@ -32,6 +32,18 @@ export const SettingsModel = (models[ModelNames.SETTINGS] ??
 
 const SETTINGS_DEFAULT = new SettingsModel({
   locked: false,
+  activeSchoolDays: [1, 2, 3, 4, 5],
+  activeSchoolTime: {
+    start: [{ days: [1, 2, 3, 4, 5], value: new Date("1970-01-01T07:00:00.000Z") }],
+    end: [
+      { days: [1, 2, 3, 4], value: new Date("1970-01-01T13:00:00.000Z") },
+      { days: [5], value: new Date("1970-01-01T10:30:00.000Z") },
+    ],
+  },
+  periodDurations: {
+    idle: { max: 30, min: 0 },
+    subject: { max: 135, min: 45 },
+  },
 });
 
 SETTINGS_DEFAULT.save({ validateBeforeSave: true }, function (err) {
