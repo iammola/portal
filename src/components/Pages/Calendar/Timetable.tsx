f9fa27359ad624f3a87590541a9a10b87f291ec2
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-14 18:07:05 +0100

    ✨feat (timetable): add periods popover

--- src/components/Pages/Calendar/Timetable.tsx
+++ src/components/Pages/Calendar/Timetable.tsx
@@ -1,4 +1,7 @@
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import { differenceInMinutes, lightFormat } from "date-fns";
+
+import { Avatar } from "components/Avatar";
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -71,24 +74,58 @@ export const TimetableWeekPanel: React.FC<{
               const duration = differenceInMinutes(end, start);
 
               return (
-                <div
-                  key={idx}
-                  className="absolute z-[1] w-full min-w-0 rounded-lg bg-gray-3 px-2 py-1.5 text-gray-12 dark:bg-gray-dark-3 dark:text-gray-dark-12"
-                  style={{
-                    top: `${(offset / 60) * 1e2}%`,
-                    height: `${(duration / 60) * 1e2}%`,
-                    width: `${100 / periodsInHour.length}%`,
-                    left: `${(100 / periodsInHour.length) * idx}%`,
-                  }}
-                >
-                  <div className="truncate text-sm tracking-wide text-gray-12 dark:text-gray-dark-12">
-                    {period._type === "subject" ? period.subject.name : period.title}
-                  </div>
-                  <div className="truncate text-xs font-medium tracking-wide text-gray-11 dark:text-gray-dark-11">
-                    {lightFormat(start, "hh:mm a")} - {lightFormat(end, "hh:mm a")}
-                    {period._type === "subject" && <> &middot; {period.teacher.name}</>}
-                  </div>
-                </div>
+                <TooltipPrimitive.Root key={idx}>
+                  <TooltipPrimitive.Trigger
+                    className="absolute z-[1] w-full min-w-0 overflow-hidden rounded-lg bg-gray-3 px-2 py-1.5 focus:outline-none focus:ring-2 focus:ring-gray-7 focus:hover:ring-gray-8 dark:bg-gray-dark-3 dark:focus:ring-gray-dark-7 dark:focus:hover:ring-gray-dark-8"
+                    style={{
+                      top: `${(offset / 60) * 1e2}%`,
+                      height: `${(duration / 60) * 1e2}%`,
+                      width: `${100 / periodsInHour.length}%`,
+                      left: `${(100 / periodsInHour.length) * idx}%`,
+                    }}
+                  >
+                    <div className="h-full text-left">
+                      <div className="truncate text-sm tracking-wide text-gray-12 dark:text-gray-dark-12">
+                        {period._type === "subject" ? period.subject.name : period.title}
+                      </div>
+                      <div className="truncate text-xs font-medium tracking-wide text-gray-11 dark:text-gray-dark-11">
+                        {lightFormat(start, "hh:mm a")} - {lightFormat(end, "hh:mm a")}
+                        {period._type === "subject" && <> &middot; {period.teacher.name}</>}
+                      </div>
+                    </div>
+                  </TooltipPrimitive.Trigger>
+                  <TooltipPrimitive.Content
+                    sideOffset={3}
+                    className="flex w-[270px] flex-col items-start justify-start gap-0.5 rounded-md bg-white p-4 shadow-md dark:bg-gray-dark-2 xs:w-[300px]"
+                  >
+                    <div className="font-semibold tracking-wide text-gray-12 dark:text-gray-dark-12">
+                      {period._type === "subject" ? period.subject.name : period.title}
+                    </div>
+                    <div className="mb-1.5 text-sm tracking-wide text-gray-11 dark:text-gray-dark-11">
+                      {lightFormat(start, "hh:mm a")} - {lightFormat(end, "hh:mm a")}
+                    </div>
+                    {"description" in period && (
+                      <div>
+                        <span className="text-xs font-medium text-gray-11 dark:text-gray-dark-11">Description</span>
+                        <p className="text-sm tracking-wide text-gray-12 dark:text-gray-dark-12">
+                          {period.description}
+                        </p>
+                      </div>
+                    )}
+                    {"teacher" in period && (
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={`/link/to/${period.teacher.username}`}
+                        className="flex w-full items-center justify-start gap-3  rounded-md p-2 hover:bg-gray-4 dark:hover:bg-gray-dark-4"
+                      >
+                        <Avatar {...period.teacher} src={period.teacher.avatar ?? ""} />
+                        <div className="truncate text-sm tracking-wide">{period.teacher.name}</div>
+                      </a>
+                    )}
+                    <TooltipPrimitive.Arrow className="fill-white dark:fill-gray-dark-3" />
+                  </TooltipPrimitive.Content>
+                </TooltipPrimitive.Root>
               );
             })}
           </div>
