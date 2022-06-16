29bcafb3d375505d48ddcc13b6bb26dded269e05
Ademola Adedeji <a.mola.dev@gmail.com>
2022-06-16 10:30:21 +0100

    🚧wip (api): return JWT payload after authentication

--- src/api/server/auth.ts
+++ src/api/server/auth.ts
@@ -9,17 +9,19 @@ import type { NextApiRequest } from "next";
 import type { NextRequest } from "next/server";
 
 /**
- * It verifies that the JWT token is valid by using the public key stored in the cookie
+ * It takes a request object and a token, and returns the payload of the JWT token if it's valid
  * @param {NextApiRequest | NextRequest} request - NextApiRequest | NextRequest - This is the request
- * object that Next.js provides.
- * @param {string} token - The token to verify
+ * object from Next.js.
+ * @param {string} token - The JWT token to verify
+ * @returns The payload of the JWT
  */
 export async function verifyAuth(request: NextApiRequest | NextRequest, token: string) {
   const key = request.cookies[JWT_COOKIE_KEY];
   if (!key || !token) throw new UnauthorizedError("Missing Authentication Token");
 
   try {
-    await jwtVerify(token, await importSPKI(key, JWT_ALG));
+    const { payload } = await jwtVerify(token, await importSPKI(key, JWT_ALG));
+    return payload;
   } catch (error: unknown) {
     throw new UnauthorizedError("Invalid Authorization Token");
   }
